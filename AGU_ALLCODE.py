63109b4f538f01760926a796373bb6b0277cd204
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-05 20:58:37 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -7,7 +7,6 @@ import talib as ta
 from email_util import *
 import common
 
-
 def strategy():
     all_code = ts.get_stock_basics()
     all_code_index = all_code[1:-1].index
@@ -20,7 +19,6 @@ def strategy():
         data_history = ts.get_k_data(codeItem, ktype='D')
 
         try:
-            # 02、 数据格式处理、并计算布林线值
             closeArray = num.array(data_history['close'])
             highArray = num.array(data_history['high'])
             lowArray = num.array(data_history['low'])
@@ -32,15 +30,10 @@ def strategy():
             ma10 = ta.SMA(num.asarray(doubleCloseArray, dtype='double'), timeperiod=10)
             ma20 = ta.SMA(num.asarray(doubleCloseArray, dtype='double'), timeperiod=20)
 
-
-            # # macd 为快线 macdsignal为慢线，macdhist为柱体
-            # macd, macdsignal, macdhist = ta.MACD(num.asarray(doubleCloseArray, dtype='double'), fastperiod=12, slowperiod=26,
-            #                                      signalperiod=9)
-
             if (ma5[-1] > ma5[-2] and ma10[-1] > ma10[-2] and ma20[-1] > ma20[-2]):
                 if (ma5[-2] > ma5[-3] and ma10[-2] > ma10[-3] and ma20[-2] > ma20[-3]):
                     continue
-                strResult += "编号:" + codeItem + "名称：" + common.gupiaomingcheng(codeItem) + "涨跌幅：" + common.zhangdiefu(codeItem) + "<br></br>"
+                strResult += "编号:" + codeItem + "名称：" + common.gupiaomingcheng(codeItem) + "涨跌幅：" + common.zhangdiefu(codeItem) + "<br>"
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
     return strResult
