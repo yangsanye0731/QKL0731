334731912ac97621c03247c6c9f4c088251d74ae
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-06 20:51:23 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -7,7 +7,7 @@ import talib as ta
 from email_util import *
 import common
 
-def strategy():
+def strategy(zhouqi):
     all_code = ts.get_stock_basics()
     all_code_index = all_code[1:-1].index
     count = 0
@@ -16,27 +16,39 @@ def strategy():
     strResult = ""
     for codeItem in all_code_index_x:
         count = count + 1
-        data_history = ts.get_k_data(codeItem, ktype='D')
+        data_history = ts.get_k_data(codeItem, ktype=zhouqi)
 
         try:
             closeArray = num.array(data_history['close'])
-            highArray = num.array(data_history['high'])
-            lowArray = num.array(data_history['low'])
+            # highArray = num.array(data_history['high'])
+            # lowArray = num.array(data_history['low'])
 
             doubleCloseArray = num.asarray(closeArray, dtype='double')
-            doubleHighArray = num.asarray(highArray, dtype='double')
-            doubleLowArray = num.asarray(lowArray, dtype='double')
-            ma5 = ta.SMA(num.asarray(doubleCloseArray, dtype='double'), timeperiod=5)
-            ma10 = ta.SMA(num.asarray(doubleCloseArray, dtype='double'), timeperiod=10)
-            ma20 = ta.SMA(num.asarray(doubleCloseArray, dtype='double'), timeperiod=20)
+            # doubleHighArray = num.asarray(highArray, dtype='double')
+            # doubleLowArray = num.asarray(lowArray, dtype='double')
+
+            # 均线
+            ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
+            ma10 = ta.SMA(doubleCloseArray, timeperiod=10)
+            ma20 = ta.SMA(doubleCloseArray, timeperiod=20)
 
             if (ma5[-1] > ma5[-2] and ma10[-1] > ma10[-2] and ma20[-1] > ma20[-2]):
                 if (ma5[-2] > ma5[-3] and ma10[-2] > ma10[-3] and ma20[-2] > ma20[-3]):
                     continue
-                strResult += "编号:" + codeItem + "名称：" + common.gupiaomingcheng(codeItem) + "涨跌幅：" + common.zhangdiefu(codeItem) + "<br>"
+
+                # macd 为快线 macdsignal为慢线，macdhist为柱体
+                macd, macdsignal, macdhist = ta.MACD(doubleCloseArray, fastperiod=12, slowperiod=26, signalperiod=9)
+                strMacd = ""
+                if (macdsignal[-1] > macdsignal[-2]):
+                    strMacd = "_MACD升势"
+                strResult += codeItem + "_" + common.gupiaomingcheng(codeItem) + "_" + common.zhangdiefu(codeItem) + strMacd + "<br>"
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
     return strResult
 
-strMailResult = strategy()
-sendMail(template1(strMailResult), "四线俱升")
+strMailResult_D = strategy('D')
+sendMail(template1(strMailResult_D), "日线四线俱升")
+strMailResult_W = strategy('W')
+sendMail(template1(strMailResult_W), "周线四线俱升")
+strMailResult_M = strategy('M')
+sendMail(template1(strMailResult_M), "月线四线俱升")
