2623f113ce4778e0b818180aca94b395564bad8b
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-07 20:48:56 +0800

    init

--- AGU_ZhiShu.py
+++ AGU_ZhiShu.py
@@ -32,15 +32,62 @@ def strategy(code, name):
      return name + str15QuShi
 
 str0 = strategy("399006", "创业")
-str1 = strategy("002281", " 光迅")
-str2 = strategy("000625", " 长安")
-str3 = strategy("300136", " 信维")
-str4 = strategy("002008", " 大族")
-str5 = strategy("600498", " 烽火")
-str6 = strategy("000739", " 普洛")
-str7 = strategy("300328", " 宜安")
-str8 = strategy("300251", " 光线")
 
-content = str0 + str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8
-title = str0 + str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8
+titleTmp = ""
+str1 = strategy("002281", " 光迅")
+if (str1.endswith("买 ")):
+     titleTmp = str1 + titleTmp
+else:
+     titleTmp = titleTmp + str1
+
+str2 = strategy("000625", " 长安")
+if (str2.endswith("买 ")):
+     titleTmp = str2 + titleTmp
+else:
+     titleTmp = titleTmp + str2
+
+str3 = strategy("300136", " 信维")
+if (str3.endswith("买 ")):
+     titleTmp = str3 + titleTmp
+else:
+     titleTmp = titleTmp + str3
+
+str4 = strategy("002008", " 大族")
+if (str4.endswith("买 ")):
+     titleTmp = str4 + titleTmp
+else:
+     titleTmp = titleTmp + str4
+
+str5 = strategy("600498", " 烽火")
+if (str5.endswith("买 ")):
+     titleTmp = str5 + titleTmp
+else:
+     titleTmp = titleTmp + str5
+
+str6 = strategy("000739", " 普洛")
+if (str6.endswith("买 ")):
+     titleTmp = str6 + titleTmp
+else:
+     titleTmp = titleTmp + str6
+
+str7 = strategy("300328", " 宜安")
+if (str7.endswith("买 ")):
+     titleTmp = str7 + titleTmp
+else:
+     titleTmp = titleTmp + str7
+
+str8 = strategy("300251", " 光线")
+if (str8.endswith("买 ")):
+     titleTmp = str8 + titleTmp
+else:
+     titleTmp = titleTmp + str8
+
+str9 = strategy("300059", " 东方")
+if (str9.endswith("买 ")):
+     titleTmp = str9 + titleTmp
+else:
+     titleTmp = titleTmp + str9
+
+content = str0 + titleTmp
+title = str0 + titleTmp
 sendMail (content, title)
