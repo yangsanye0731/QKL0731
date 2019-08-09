0f4aa39054d9ff0b59d4eb0b5816caa252c22e61
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-09 21:28:22 +0800

    init

--- QKL_TRXUSDT.py
+++ QKL_TRXUSDT.py
@@ -394,12 +394,14 @@ def strategy(name,zhouqi):
 title0, content0 = strategy("BTC/USDT","1h")
 title1, content1 = strategy("ETH/USDT","1h")
 title2, content2 = strategy("EOS/USDT","1h")
-title3, content3 = strategy("HT/USDT","1h")
+title3, content3 = strategy("LTC/USDT","1h")
+title4, content4 = strategy("HT/USDT","1h")
+title5, content5 = strategy("XRP/USDT","1h")
 
 mulu1 = "=================================<br>"
 mulu2 = "===========圈=子=日=报============<br>"
 mulu3 = "=================================<br>"
-content = mulu1 + mulu2 + mulu3 + content0 + "<br><hr>" +  content1 + "<br><hr>" + content2 + "<br><hr>" + content3
+content = mulu1 + mulu2 + mulu3 + content0 + "<br><hr>" +  content1 + "<br><hr>" + content2 + "<br><hr>" + content3 + "<br><hr>" + content4 + "<br><hr>" + content4
 title = title0 + title1 + title2 + title3
 
 sendMail(content, title)
