e77231fc8f89ce9a46fa6c051f97e22bb74930b7
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-10 17:00:09 +0800

    init

--- AGU_ZhiShu.py
+++ AGU_ZhiShu.py
@@ -29,78 +29,88 @@ def strategy(code, name):
           str15QuShi = "空 "
 
      print(name + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-     return name + str15QuShi
+     titile = name + str15QuShi
+
+     content = name + str15QuShi
+     return title, content
 
 str0 = strategy("399006", "※均线形态创业")
 
 titleTmp = ""
-str1 = strategy("002281", " 光迅")
+str1, content1 = strategy("002281", " 光迅")
 if (str1.endswith("买 ")):
      titleTmp = str1 + titleTmp
 else:
      titleTmp = titleTmp + str1
 
-str2 = strategy("000625", " 长安")
+str2, content2 = strategy("000625", " 长安")
 if (str2.endswith("买 ")):
      titleTmp = str2 + titleTmp
 else:
      titleTmp = titleTmp + str2
 
-str3 = strategy("300136", " @信维")
+str3, content3 = strategy("300136", " @信维")
 if (str3.endswith("买 ")):
      titleTmp = str3 + titleTmp
 else:
      titleTmp = titleTmp + str3
 
-str4 = strategy("002008", " 大族")
+str4, content4 = strategy("002008", " 大族")
 if (str4.endswith("买 ")):
      titleTmp = str4 + titleTmp
 else:
      titleTmp = titleTmp + str4
 
-str5 = strategy("600498", " 烽火")
+str5, content5 = strategy("600498", " 烽火")
 if (str5.endswith("买 ")):
      titleTmp = str5 + titleTmp
 else:
      titleTmp = titleTmp + str5
 
-str6 = strategy("000739", " 普洛")
+str6, content6 = strategy("000739", " 普洛")
 if (str6.endswith("买 ")):
      titleTmp = str6 + titleTmp
 else:
      titleTmp = titleTmp + str6
 
-str7 = strategy("300328", " 宜安")
+str7, content7 = strategy("300328", " 宜安")
 if (str7.endswith("买 ")):
      titleTmp = str7 + titleTmp
 else:
      titleTmp = titleTmp + str7
 
-str8 = strategy("300251", " 光线")
+str8, content8 = strategy("300251", " 光线")
 if (str8.endswith("买 ")):
      titleTmp = str8 + titleTmp
 else:
      titleTmp = titleTmp + str8
 
-str9 = strategy("300059", " 东方")
+str9, content9 = strategy("300059", " 东方")
 if (str9.endswith("买 ")):
      titleTmp = str9 + titleTmp
 else:
      titleTmp = titleTmp + str9
 
-str10 = strategy("300584", " @海辰")
+str10, content10 = strategy("300584", " @海辰")
 if (str10.endswith("买 ")):
      titleTmp = str10 + titleTmp
 else:
      titleTmp = titleTmp + str10
 
-str11 = strategy("300664", " @鹏鹞")
+str11, content11 = strategy("300664", " @鹏鹞")
 if (str11.endswith("买 ")):
      titleTmp = str11 + titleTmp
 else:
      titleTmp = titleTmp + str11
 
 
-content = str0 + titleTmp
 title = str0 + titleTmp
+
+mulu1 = "=================================<br>"
+mulu2 = "=圈=子=日=报=：" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "<br>"
+mulu3 = "=================================<br>"
+content = mulu1 + mulu2 + mulu3 + content1  + "<br><hr>" + content2 + "<br><hr>" + content3 + "<br><hr>" \
+          + content4 + "<br><hr>" + content5 + "<br><hr>" + content6 + "<br><hr>" + content7 + "<br><hr>" \
+          + content8 + "<br><hr>" + content9 + "<br><hr>" + content10 + "<br><hr>" + content11
+
 sendMail (content, title)
