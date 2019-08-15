b187249ab836c2bdf1d30413fa6226f593b7a0d9
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-15 10:01:27 +0800

    init

--- AGU_ZhiShu.py
+++ AGU_ZhiShu.py
@@ -23,7 +23,7 @@ def strategy(code, name, fullName):
      SMA30_15_10 = ta.SMA(doubleCloseArray, timeperiod=10)
      SMA30_15_20 = ta.SMA(doubleCloseArray, timeperiod=20)
      SMA30_15_30 = ta.SMA(doubleCloseArray, timeperiod=30)
-
+     xingtai = ""
      if (SMA30_15_5[-1] > SMA30_15_10[-1] > SMA30_15_20[-1] > SMA30_15_30[-1]):
           xingtai = "上好"
 
