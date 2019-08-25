0159309dffc532c495344b8fbf61868d644e6edd
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-25 21:18:50 +0800

    init

--- AGU_SHUJUFENXI.py
+++ AGU_SHUJUFENXI.py
@@ -58,7 +58,7 @@ array3 = []
 
 def zhou_chuang_xin_gao_count(curDate):
     all_code = ts.get_stock_basics()
-    all_code_index = all_code[1:5].index
+    all_code_index = all_code[1:-1].index
     count = 0
     count2 = 0
     all_code_index_x = num.array(all_code_index)
