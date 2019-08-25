fe644fb74149acfd888c316e5145301ca782b88c
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-25 18:23:51 +0800

    init

--- AGU_ZhiShu.py
+++ AGU_ZhiShu.py
@@ -304,6 +304,13 @@ titleTmp, contentTmp = pinjie(str20, titleTmp, content20, contentTmp)
 str20, content20 = strategy("002074", "国轩", "国轩高科")
 titleTmp, contentTmp = pinjie(str20, titleTmp, content20, contentTmp)
 
+str20, content20 = strategy("600271", "航天", "航天信息（电子发票）")
+titleTmp, contentTmp = pinjie(str20, titleTmp, content20, contentTmp)
+
+str20, content20 = strategy("000997", "新大陆", "新大陆（电子发票）")
+titleTmp, contentTmp = pinjie(str20, titleTmp, content20, contentTmp)
+
+
 title = str0 + str00 + titleTmp
 mulu = "# **<font color=#FF0000 size=6 face=\"微软雅黑\">每日简报 " + time.strftime("%m-%d %H:%M", time.localtime()) + "</font>**\n\n"
 content = mulu + content0 + "***\n\n***\n\n" + content00 + "***\n\n***\n\n" + "***\n\n***\n\n" + contentTmp
