bf5ce1b860bfacb9d17d9274f6e7c3861f443a03
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-08-26 19:32:44 +0800

    init

--- AGU_SHUJUFENXI.py
+++ AGU_SHUJUFENXI.py
@@ -63,47 +63,51 @@ def zhou_chuang_xin_gao_count(curDate):
     return count2,doubleCloseArray_chuagnyeban[-1]
 
 
-countx9 = zhou_chuang_xin_gao_count("2019-06-28")
-countx10 = zhou_chuang_xin_gao_count("2019-06-21")
-countx11 = zhou_chuang_xin_gao_count("2019-06-14")
-countx12 = zhou_chuang_xin_gao_count("2019-06-07")
-countx8 = zhou_chuang_xin_gao_count("2019-07-05")
-countx7 = zhou_chuang_xin_gao_count("2019-07-12")
-countx6 = zhou_chuang_xin_gao_count("2019-07-19")
-countx5 = zhou_chuang_xin_gao_count("2019-07-26")
-# countx4 = zhou_chuang_xin_gao_count("20190802")
-# countx3 = zhou_chuang_xin_gao_count("20190809")
-# countx2 = zhou_chuang_xin_gao_count("20190816")
-# countx1 = zhou_chuang_xin_gao_count("20190823")
 
-
-
-
-
-
-
-# countx13 = zhou_chuang_xin_gao_count("2019-05-31")
-# countx14 = zhou_chuang_xin_gao_count("2019-05-24")
-# countx15 = zhou_chuang_xin_gao_count("2019-05-17")
-# countx16 = zhou_chuang_xin_gao_count("2019-05-10")
-# countx17 = zhou_chuang_xin_gao_count("2019-05-03")
-# countx18 = zhou_chuang_xin_gao_count("2019-04-26")
-# countx19 = zhou_chuang_xin_gao_count("2019-04-19")
-# countx20 = zhou_chuang_xin_gao_count("2019-04-12")
-# countx21 = zhou_chuang_xin_gao_count("2019-04-05")
-# countx22 = zhou_chuang_xin_gao_count("2019-03-29")
-# countx23 = zhou_chuang_xin_gao_count("2019-03-22")
-# countx24 = zhou_chuang_xin_gao_count("2019-03-15")
-# countx25 = zhou_chuang_xin_gao_count("2019-03-08")
-# countx26 = zhou_chuang_xin_gao_count("2019-03-01")
-# countx27 = zhou_chuang_xin_gao_count("2019-02-22")
-# countx28 = zhou_chuang_xin_gao_count("2019-02-15")
-# countx29 = zhou_chuang_xin_gao_count("2019-02-08")
-# countx30 = zhou_chuang_xin_gao_count("2019-02-01")
-# countx31 = zhou_chuang_xin_gao_count("2019-01-25")
-# countx32 = zhou_chuang_xin_gao_count("2019-01-18")
-# countx33 = zhou_chuang_xin_gao_count("2019-01-11")
 # countx34 = zhou_chuang_xin_gao_count("2019-01-04")
+# countx33 = zhou_chuang_xin_gao_count("2019-01-11")
+# countx32 = zhou_chuang_xin_gao_count("2019-01-18")
+# countx31 = zhou_chuang_xin_gao_count("2019-01-25")
+# countx30 = zhou_chuang_xin_gao_count("2019-02-01")
+# countx29 = zhou_chuang_xin_gao_count("2019-02-08")
+# countx28 = zhou_chuang_xin_gao_count("2019-02-15")
+# countx27 = zhou_chuang_xin_gao_count("2019-02-22")
+# countx26 = zhou_chuang_xin_gao_count("2019-03-01")
+# countx25 = zhou_chuang_xin_gao_count("2019-03-08")
+# countx24 = zhou_chuang_xin_gao_count("2019-03-15")
+# countx23 = zhou_chuang_xin_gao_count("2019-03-22")
+# countx22 = zhou_chuang_xin_gao_count("2019-03-29")
+# countx21 = zhou_chuang_xin_gao_count("2019-04-05")
+# countx20 = zhou_chuang_xin_gao_count("2019-04-12")
+# countx19 = zhou_chuang_xin_gao_count("2019-04-19")
+# countx18 = zhou_chuang_xin_gao_count("2019-04-26")
+# countx17 = zhou_chuang_xin_gao_count("2019-05-03")
+# countx16 = zhou_chuang_xin_gao_count("2019-05-10")
+# countx15 = zhou_chuang_xin_gao_count("2019-05-17")
+# countx14 = zhou_chuang_xin_gao_count("2019-05-24")
+# countx13 = zhou_chuang_xin_gao_count("2019-05-31")
+# countx12 = zhou_chuang_xin_gao_count("2019-06-07")
+# countx11 = zhou_chuang_xin_gao_count("2019-06-14")
+# countx10 = zhou_chuang_xin_gao_count("2019-06-21")
+# countx9 = zhou_chuang_xin_gao_count("2019-06-28")
+# countx8 = zhou_chuang_xin_gao_count("2019-07-05")
+# countx7 = zhou_chuang_xin_gao_count("2019-07-12")
+# countx6 = zhou_chuang_xin_gao_count("2019-07-19")
+# countx5 = zhou_chuang_xin_gao_count("2019-07-26")
+# countx4 = zhou_chuang_xin_gao_count("2019-08-02")
+# countx3 = zhou_chuang_xin_gao_count("2019-08-09")
+countx2 = zhou_chuang_xin_gao_count("2019-08-16")
+countx1 = zhou_chuang_xin_gao_count("2019-08-23")
+
+
+
+
+
+
+
+
+
+
 
 
 common_image.plt_image_2(array1, array2, array3)
@@ -111,5 +115,4 @@ common_image.plt_image_2(array1, array2, array3)
 print(array1)
 print(array2)
 print(array3)
-
-# common.dingding_markdown_msg_2("aaa", array1)
+common.dingding_markdown_msg_2("新高执行完成", "新高执行完成")
