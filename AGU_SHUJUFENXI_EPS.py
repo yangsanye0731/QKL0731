bbbd0a1a4113a4d4a9891b963c56128a125969ca
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-09-04 20:58:28 +0800

    init

--- AGU_SHUJUFENXI_EPS.py
+++ AGU_SHUJUFENXI_EPS.py
@@ -36,7 +36,6 @@ def code_eps():
                 SMA_W_5 = ta.SMA(data_history_W, timeperiod=5)
                 if (doubleCloseArray_W[-1] < SMA_W_5[-1]):
                     strResult = strResult + common.codeName(codeItem) + "本期EPS增长50%以上，但当前价格在5周线以下\n\n"
-                time.sleep(3)
             if (eps_2 > 50 and yingyeup_2 > 20):
                 print(common.codeName(codeItem) + ",EPS:" + "%.1f" % epsup_2 + " LYL:" + "%.1f" % yingyeup_2)
                 strResult_2 = strResult_2 + common.codeName(codeItem) + ",EPS:" + "%.1f" % epsup_2 + " LYL:" + "%.1f" % yingyeup_2 + "\n\n"
@@ -48,8 +47,8 @@ def code_eps():
                 SMA_W_5 = ta.SMA(data_history_W, timeperiod=5)
                 if (doubleCloseArray_W[-1] < SMA_W_5[-1]):
                     strResult_2 = strResult_2 + common.codeName(codeItem) + "上期EPS增长50%以上，但当前价格在5周线以下\n\n"
-                time.sleep(3)
 
+            time.sleep(2)
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
 
