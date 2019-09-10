3b5451d53ce976b088e4a29dd7c64f713ce79e39
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-09-10 08:28:18 +0800

    init

--- AGU_SHUJUFENXI_EPS.py
+++ AGU_SHUJUFENXI_EPS.py
@@ -9,6 +9,52 @@ import common_image
 import datetime
 from email_util import *
 
+def strategy(zhouqi):
+    all_code = ts.get_stock_basics()
+    all_code_index = all_code[1:-1].index
+    count = 0
+    all_code_index_x = num.array(all_code_index)
+
+    strResult = ""
+    strResult_2 = ""
+    for codeItem in all_code_index_x:
+        count = count + 1
+        print(count)
+        data_history = ts.get_k_data(codeItem, ktype=zhouqi)
+
+        try:
+            closeArray = num.array(data_history['close'])
+            doubleCloseArray = num.asarray(closeArray, dtype='double')
+
+            highArray = num.array(data_history['high'])
+            doubleHighArray = num.asarray(highArray, dtype='double')
+
+            openArray = num.array(data_history['open'])
+            doubleOpenArray = num.asarray(openArray, dtype='double')
+
+            # 均线
+            ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
+            eps, epsup, yingyeup, eps_2, epsup_2, yingyeup_2 = common.codeEPS(codeItem)
+            codeName = common.codeName(codeItem)
+            # print(codeItem)
+            # print(doubleCloseArray[-1])
+            # print(ma5[-1])
+            if (doubleHighArray[-1] > ma5[-1] and doubleOpenArray[-1] < ma5[-1] and epsup > 0 and yingyeup > 0):
+                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "======================================" + codeItem)
+                common_image.plt_image_kuaYueWeek5Line(codeItem, codeName, "W", "%.1f" % epsup, "%.1f" % yingyeup)
+                strResult += common.codeName(codeItem) + "跨越五周线" + "<br>"
+
+            if (ma5[-1] < ma5[-2] and ma5[-2] < ma5[-3] and ma5[-3] < ma5[-4] and epsup > 0 and yingyeup > 0 ):
+                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "======================================" + codeItem)
+                common_image.plt_image_lianXuXiaJiangWeek5Line(codeItem, codeName, "W", "%.1f" % epsup, "%.1f" % yingyeup)
+                strResult_2 += common.codeName(codeItem) + "5周线连续下降" + "<br>"
+
+            time.sleep(2)
+
+        except (IOError, TypeError, NameError, IndexError, Exception) as e:
+            print(e)
+    return strResult, strResult_2
+
 # EPS分析结果
 def code_eps():
     all_code = ts.get_stock_basics()
@@ -56,6 +102,11 @@ def code_eps():
             print(e)
 
     return strResult, strResult_2
+
+strMailResult_W, strResult_2 = strategy('W')
+sendMail(template1(strMailResult_W), "跨域5周线")
+time.sleep(10)
+sendMail(template1(strResult_2), "5周线连续下降")
 
 re1, re2 = code_eps()
 sendMail(template1(re1), "EPS分析结果已输出")
