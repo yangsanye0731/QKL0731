07f7cb1f294b3495ea444ea03cdd059ce66e3222
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-09-12 09:59:01 +0800

    init

--- common_image.py
+++ common_image.py
@@ -177,7 +177,7 @@ def plt_image_baiRiXinGao(xinGaoGeShu, zhiShuShuJu, riQi):
 
 # 5周线连续下降
 def plt_image_lianXuXiaJiangWeek5Line(code, codeName, type, eps, yoy):
-    myfont = matplotlib.font_manager.FontProperties(fname="/root/software/QKL/simsun.ttc", size="25")
+    myfont = matplotlib.font_manager.FontProperties(fname="/root/software/QKL/simsun.ttc", size="20")
     ts = tushare.get_k_data(code, ktype = type)
     ts=ts[["open","close","high","low","volume"]]
 
@@ -200,10 +200,11 @@ def plt_image_lianXuXiaJiangWeek5Line(code, codeName, type, eps, yoy):
     # plt.plot(avg_30,color="b")
     plt.xticks(rotation=75)
     #设置坐标轴名称
+    timeStr1 = time.strftime("%Y%m%d", time.localtime())
     if (type == "W"):
-        plt.title(codeName + '(' + code  + ')5周线连续下降,EPS:' + eps + ",营业额：" + yoy, fontproperties=myfont)
+        plt.title(timeStr1 + "_" + codeName + '(' + code  + ')5周线连续下降,EPS:' + eps + "%,营业额：" + yoy + "%", fontproperties=myfont)
     plt.xlabel('日期', fontproperties=myfont)
-    plt.ylabel('价格' + common.zhangdiefu(code), fontproperties=myfont)
+    plt.ylabel('价格 ' + common.zhangdiefu(code), fontproperties=myfont)
 
     #设置坐标轴范围
     changdu = len(ts)
@@ -213,13 +214,11 @@ def plt_image_lianXuXiaJiangWeek5Line(code, codeName, type, eps, yoy):
     if (changdu > 550):
         plt.xlim(400, changdu)
 
-    timeStr1 = time.strftime("%Y%m%d", time.localtime())
-    timeStr2 = time.strftime("%m%d%H%M", time.localtime())
     path = "./images/" + timeStr1 + "/lianXuXiaJiangWeek5Line"
     if not os.path.exists(path):
         os.makedirs(path)
 
-    plt.savefig(path + "/" +  code + "_" + codeName + ".png")
+    plt.savefig(path + "/" +  timeStr1 + "_" + codeName + ".png")
     plt.close()
 
 # plt_image("399006", "创业板指", "30")
