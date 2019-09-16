4a10bdb1cc8b60552c6a2381bb9e04b34138bb63
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-09-16 20:37:52 +0800

    init

--- AGU_ZhiShu.py
+++ AGU_ZhiShu.py
@@ -222,7 +222,7 @@ def strategy(code, name, fullName):
 # 买 表示：30分钟 5、10、20、30均线 均处于向上态势
 def pinjie(title, titleTmp, content, contentTmp):
      # if (("趋势" in title or "上好" in title) and "买" in title):
-     if ("买" in title):
+     if ("买1" in title or "买2" in title):
           titleTmp = title + titleTmp
           contentTmp = content + "***\n\n" + contentTmp
      # else:
