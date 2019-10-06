ec58b01ffcf2062d30fb665c60cacf78053bb9b9
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-10-06 18:34:41 +0800

    init

--- HUICE_30Junxian_20Junxian.py
+++ HUICE_30Junxian_20Junxian.py
@@ -4,9 +4,9 @@ from __future__ import (absolute_import, division, print_function,
 import backtrader as bt
 import tushare as ts
 import datetime
+import numpy as num
 
-# Create a Stratey
-class TestStrategy(bt.Strategy):
+class QushiStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
@@ -17,8 +17,8 @@ class TestStrategy(bt.Strategy):
         self.dataclose = self.datas[0].close
         # To keep track of pending orders
         self.order = None
-        self.sma5 = bt.indicators.SimpleMovingAverage(self.datas[0], period=5)
-        self.sma10 = bt.indicators.SimpleMovingAverage(self.datas[0], period=10)
+        # self.sma5 = bt.indicators.SimpleMovingAverage(self.datas[0], period=5)
+        # self.sma10 = bt.indicators.SimpleMovingAverage(self.datas[0], period=10)
         self.sma20 = bt.indicators.SimpleMovingAverage(self.datas[0], period=20)
         self.sma30 = bt.indicators.SimpleMovingAverage(self.datas[0], period=30)
         self.bull = bt.indicators.BollingerBands(self.datas[0])
@@ -43,13 +43,12 @@ class TestStrategy(bt.Strategy):
         self.order = None
 
     def next(self):
-        # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
         # self.log('SMA5, %.2f' % self.sma5[0])
         # self.log('SMA10, %.2f' % self.sma10[0])
         # self.log('SMA20, %.2f' % self.sma20[0])
         # self.log('SMA30, %.2f' % self.sma30[0])
-        self.log('BULL TOP, %.2f' % self.bull.top[0])
+        # self.log('BULL TOP, %.2f' % self.bull.top[0])
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
@@ -57,49 +56,50 @@ class TestStrategy(bt.Strategy):
 
         # Check if we are in the market
         if not self.position:
-            if (self.sma30[0] > self.sma30[-1] and self.sma30[-2] > self.sma30[-1]):
-                self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                self.order = self.buy(size=10000)
-
+            if (self.sma30[0] > self.sma30[-1]):
+            # if (self.MACD.macd[0] > self.MACD.macd[-1]):
+                if(self.dataclose[0] > self.sma20[0] and self.datas[0].open < self.sma20[0] and self.dataclose[-1] < self.sma20[-1]):
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                    self.order = self.buy(size=10000)
         else:
-            if (self.sma20[0] < self.sma20[-1]):
+            if (self.datas[0].close > self.bull.top[0]):
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                self.order = self.sell(size=10000)
+            if (self.datas[0].close > self.bull.top[0]):
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
                 self.order = self.sell(size=10000)
 
 if __name__ == '__main__':
-    # 创建回测
-    cerebro = bt.Cerebro()
-    # 设置交易费用
-    cerebro.broker.setcommission(commission=0.001)
-    #  添加策略
-    cerebro.addstrategy(TestStrategy)
+    all_code = ts.get_stock_basics()
+    all_code_index = all_code[1:-1].index
+    count = 0
+    all_code_index_x = num.array(all_code_index)
 
-    # 60分钟数据
-    datad = ts.get_k_data("300023", ktype='60', start="2018-01-01")
-    datad['datetime'] = datad['date'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M'))
-    datad['openInterest'] = 0
-    datad = datad[["open","close","high","low","volume","datetime","openInterest"]]
-    datad.set_index("datetime",inplace=True)
-    add_data_60 = bt.feeds.PandasData(dataname=datad)
-    print(add_data_60)
+    strResult = ""
+    for codeItem in all_code_index_x:
+        # 创建回测
+        cerebro = bt.Cerebro()
+        # 设置交易费用
+        cerebro.broker.setcommission(commission=0.001)
+        #  添加策略
+        cerebro.addstrategy(QushiStrategy)
 
-    # 日线数据
-    data_D = ts.get_k_data("300023", ktype='D', start="2018-01-01")
-    data_D['datetime'] = data_D['date'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
-    data_D['openInterest'] = 0
-    data_D = data_D[["open", "close", "high", "low", "volume", "datetime", "openInterest"]]
-    data_D.set_index("datetime", inplace=True)
-    add_data_D = bt.feeds.PandasData(dataname=data_D)
-    print(add_data_D)
+        # 日线数据
+        data_D = ts.get_k_data(codeItem, ktype='D', start="2018-01-01")
+        data_D['datetime'] = data_D['date'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+        data_D['openInterest'] = 0
+        data_D = data_D[["open", "close", "high", "low", "volume", "datetime", "openInterest"]]
+        data_D.set_index("datetime", inplace=True)
+        add_data_D = bt.feeds.PandasData(dataname=data_D)
+        print(add_data_D)
 
-    # 添加数据
-    # cerebro.adddata(add_data_60)
-    cerebro.adddata(add_data_D)
+        # 添加数据
+        cerebro.adddata(add_data_D)
 
-    # 设置初始账户
-    cerebro.broker.setcash(300000.0)
+        # 设置初始账户
+        cerebro.broker.setcash(300000.0)
 
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.run()
-    cerebro.plot()
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        cerebro.run()
+        cerebro.plot()
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
