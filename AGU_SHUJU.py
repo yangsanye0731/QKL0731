e6c55411d134268f0036d7d3369fb0f0c0d620ad
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-10-16 20:12:32 +0800

    init

--- AGU_SHUJU.py
+++ AGU_SHUJU.py
@@ -14,11 +14,11 @@ def strategy(type):
 
     # 获取实时数据
     data1 = common_mysqlUtil.selectCountRecord(type)
-    print(data1)
+    print("实时数据：" + str(data1))
 
     # 获取已经入库的历史数据
     data2 = common_mysqlUtil.select_zhishu_count_record(type)
-    print(data2)
+    print("已入库的历史数据:" + str(data2))
 
     # 如果历史数据为空，插入数据
     print(len(data2))
@@ -27,12 +27,16 @@ def strategy(type):
         data2 = common_mysqlUtil.select_zhishu_count_record(type)
 
     # 30MIN上升数大于下降数，且上升数增加
-    if (data1[0][5] > data1[0][6] and data1[0][5] > int(data2[0][5])):
-        sendMail("30MIN上升数增加", "30MIN上升数增加，买入")
+    print("上升数：" + str(data1[0][5]))
+    print("上升数：" + str(data1[0][6]))
+    print("总数：" + str(data1[0][0]))
+    print("总数618：" + str(int(data1[0][0] * 0.6)))
+    if (data1[0][5] > int(data1[0][0] * 0.6) or data1[0][6] > int(data1[0][0] * 0.6)):
+        sendMail("30MIN上升数，下降数达到618", "30MIN上升数，下降数达到618")
 
-    # 30MIN上升数小于下降数,且下降数增加
-    if (data1[0][5] < data1[0][6] and data1[0][6] > int(data2[0][6])):
-        sendMail("30MIN下降数增加", "30MIN下降数增加，强烈卖出")
+    # # 30MIN上升数小于下降数,且下降数增加
+    # if (data1[0][5] < data1[0][6] and data1[0][6] > int(data2[0][6])):
+    #     sendMail("30MIN下降数增加", "30MIN下降数增加，强烈卖出")
 
     # 插入日志信息
     common_mysqlUtil.insert_zhishu_count_record(type)
