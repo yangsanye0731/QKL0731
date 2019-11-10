1e1a4e43061ba68084f26c90fc867b1d1287d909
yangsanye2019 <317491841@qq.com>
2019-11-10 08:27:50 +0800

    init

--- common_zhibiao.py
+++ common_zhibiao.py
@@ -11,21 +11,78 @@ import tushare as ts
 import talib as ta
 import pandas as pd
 
-def zhibiao(code, type):
-     data_history = ts.get_k_data(code, ktype=type)
+'''
+公共功能：KDJ指标
+'''
+def KDJ_zhibiao(data_history, doubleCloseArray):
+     stock_data = {}
+     low_list = data_history.low.rolling(9).min()
+     low_list.fillna(value=data_history.low.expanding().min(), inplace=True)
+     high_list = data_history.high.rolling(9).max()
+     high_list.fillna(value=data_history.high.expanding().max(), inplace=True)
+     rsv = (doubleCloseArray - low_list) / (high_list - low_list) * 100
+     stock_data['KDJ_K'] = pd.DataFrame.ewm(rsv, com=2).mean()
+     stock_data['KDJ_D'] = pd.DataFrame.ewm(stock_data['KDJ_K'], com=2).mean()
+     stock_data['KDJ_J'] = 3 * stock_data['KDJ_K'] - 2 * stock_data['KDJ_D']
+     dddd = pd.DataFrame(stock_data)
+     KDJ_J_title = "KDJ_" + "%.2f" % dddd.KDJ_J[len(dddd) - 1] + " "
+     return KDJ_J_title
 
-     closeArray = num.array(data_history['close'])
-     highArray = num.array(data_history['high'])
-     lowArray = num.array(data_history['low'])
-     openArray = num.array(data_history['open'])
 
-     doubleCloseArray = num.asarray(closeArray, dtype='double')
-     doubleHighArray = num.asarray(highArray, dtype='double')
-     doubleLowArray = num.asarray(lowArray, dtype='double')
-     doubleOpenArray = num.asarray(openArray, dtype='double')
+'''
+公共功能：BULL指标
+'''
+def BULL_zhibiao(doubleCloseArray, closeArray, lowArray):
+     upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+     BULL_middleband = ""
+     if (middleband[-1] >= middleband[-2]):
+          BULL_middleband = "布林中线趋势上升"
+     else:
+          BULL_middleband = "布林中线趋势下降"
 
-     price =  "%.2f" % doubleCloseArray[-1] + "_" + "%.2f" % doubleCloseArray[-2] + "_" + "%.2f" % doubleCloseArray[-3]
+     BULL_title = ""
+     if (closeArray[-1] > upperband[-1]):
+          BULL_title = "上穿布林线上沿"
 
+     if (lowArray[-1] < lowerband[-1] * 1.005):
+          BULL_title = "下穿布林线下沿"
+
+     return BULL_title, BULL_middleband
+
+'''
+公共功能：MACD指标
+'''
+def MACD_zhibiao(doubleCloseArray):
+     # macd 为快线 macdsignal为慢线，macdhist为柱体
+     macd, macdsignal, macdhist = ta.MACD(doubleCloseArray, fastperiod=12, slowperiod=26, signalperiod=9)
+     MACD_title = ""
+     if (macdsignal[-1] > macdsignal[-2]):
+          MACD_title = "MACD慢线上行1"
+          if (macdsignal[-2] > macdsignal[-3]):
+               MACD_title = "MACD慢线上行2"
+               if (macdsignal[-3] > macdsignal[-4]):
+                    MACD_title = "MACD慢线上行3"
+                    if (macdsignal[-4] > macdsignal[-5]):
+                         MACD_title = "MACD慢线上行4"
+                         if (macdsignal[-5] > macdsignal[-6]):
+                              MACD_title = "MACD慢线上行5"
+                              if (macdsignal[-6] > macdsignal[-7]):
+                                   MACD_title = "MACD慢线上行6"
+                                   if (macdsignal[-7] > macdsignal[-8]):
+                                        MACD_title = "MACD慢线上行7"
+                                        if (macdsignal[-8] > macdsignal[-9]):
+                                             MACD_title = "MACD慢线上行8"
+                                             if (macdsignal[-9] > macdsignal[-10]):
+                                                  MACD_title = "MACD慢线上行9"
+                                                  if (macdsignal[-11] > macdsignal[-12]):
+                                                       MACD_title = "MACD慢线上行10"
+
+     return MACD_title
+
+'''
+公共功能：均线指标
+'''
+def junxian_zhibiao(doubleCloseArray, doubleOpenArray):
      MA_5 = ta.SMA(doubleCloseArray, timeperiod=5)
      MA_10 = ta.SMA(doubleCloseArray, timeperiod=10)
      MA_20 = ta.SMA(doubleCloseArray, timeperiod=20)
@@ -81,104 +138,91 @@ def zhibiao(code, type):
 
      qushi_5_10_20_30 = ""
      if (MA_5[-1] > MA_5[-2] and MA_10[-1] > MA_10[-2] and MA_20[-1] > MA_20[-2] and MA_30[-1] > MA_30[-2]):
-         qushi_5_10_20_30 = "均线5、10、20、30齐升1"
-         if (MA_5[-2] > MA_5[-3] and MA_10[-2] > MA_10[-3] and MA_20[-2] > MA_20[-3] and MA_30[-2] > MA_30[-3]):
-              qushi_5_10_20_30 = "均线5、10、20、30齐升2"
-              if (MA_5[-3] > MA_5[-4] and MA_10[-3] > MA_10[-4] and MA_20[-3] > MA_20[-4] and MA_30[-3] > MA_30[-4]):
-                   qushi_5_10_20_30 = "均线5、10、20、30齐升3"
-                   if (MA_5[-4] > MA_5[-5] and MA_10[-4] > MA_10[-5] and MA_20[-4] > MA_20[-5] and MA_30[-4] > MA_30[-5]):
-                        qushi_5_10_20_30 = "均线5、10、20、30齐升4"
-                        if (MA_5[-5] > MA_5[-6] and MA_10[-5] > MA_10[-6] and MA_20[-5] > MA_20[-6] and MA_30[-5] > MA_30[-6]):
-                             qushi_5_10_20_30 = "均线5、10、20、30齐升5"
-                             if (MA_5[-6] > MA_5[-7] and MA_10[-6] > MA_10[-7] and MA_20[-6] > MA_20[-7] and MA_30[-6] > MA_30[-7]):
-                                  qushi_5_10_20_30 = "均线5、10、20、30齐升6"
-                                  if (MA_5[-7] > MA_5[-8] and MA_10[-7] > MA_10[-8] and MA_20[-7] > MA_20[-8] and MA_30[-7] > MA_30[-8]):
-                                       qushi_5_10_20_30 = "均线5、10、20、30齐升7"
-                                       if (MA_5[-8] > MA_5[-9] and MA_10[-8] > MA_10[-9] and MA_20[-8] > MA_20[-9] and MA_30[-8] > MA_30[-9]):
-                                            qushi_5_10_20_30 = "均线5、10、20、30齐升8"
-                                            if (MA_5[-9] > MA_5[-10] and MA_10[-9] > MA_10[-10] and MA_20[-9] > MA_20[-10] and MA_30[-9] > MA_30[-10]):
-                                                 qushi_5_10_20_30 = "均线5、10、20、30齐升9"
+          qushi_5_10_20_30 = "均线5、10、20、30齐升1"
+          if (MA_5[-2] > MA_5[-3] and MA_10[-2] > MA_10[-3] and MA_20[-2] > MA_20[-3] and MA_30[-2] > MA_30[-3]):
+               qushi_5_10_20_30 = "均线5、10、20、30齐升2"
+               if (MA_5[-3] > MA_5[-4] and MA_10[-3] > MA_10[-4] and MA_20[-3] > MA_20[-4] and MA_30[-3] > MA_30[-4]):
+                    qushi_5_10_20_30 = "均线5、10、20、30齐升3"
+                    if (MA_5[-4] > MA_5[-5] and MA_10[-4] > MA_10[-5] and MA_20[-4] > MA_20[-5] and MA_30[-4] > MA_30[
+                         -5]):
+                         qushi_5_10_20_30 = "均线5、10、20、30齐升4"
+                         if (MA_5[-5] > MA_5[-6] and MA_10[-5] > MA_10[-6] and MA_20[-5] > MA_20[-6] and MA_30[-5] >
+                                 MA_30[-6]):
+                              qushi_5_10_20_30 = "均线5、10、20、30齐升5"
+                              if (MA_5[-6] > MA_5[-7] and MA_10[-6] > MA_10[-7] and MA_20[-6] > MA_20[-7] and MA_30[
+                                   -6] > MA_30[-7]):
+                                   qushi_5_10_20_30 = "均线5、10、20、30齐升6"
+                                   if (MA_5[-7] > MA_5[-8] and MA_10[-7] > MA_10[-8] and MA_20[-7] > MA_20[-8] and
+                                           MA_30[-7] > MA_30[-8]):
+                                        qushi_5_10_20_30 = "均线5、10、20、30齐升7"
+                                        if (MA_5[-8] > MA_5[-9] and MA_10[-8] > MA_10[-9] and MA_20[-8] > MA_20[-9] and
+                                                MA_30[-8] > MA_30[-9]):
+                                             qushi_5_10_20_30 = "均线5、10、20、30齐升8"
+                                             if (MA_5[-9] > MA_5[-10] and MA_10[-9] > MA_10[-10] and MA_20[-9] > MA_20[
+                                                  -10] and MA_30[-9] > MA_30[-10]):
+                                                  qushi_5_10_20_30 = "均线5、10、20、30齐升9"
 
      if (MA_5[-1] < MA_5[-2] and MA_10[-1] < MA_10[-2] and MA_20[-1] < MA_20[-2] and MA_30[-1] < MA_30[-2]):
-         qushi_5_10_20_30 = "均线5、10、20、30齐降1"
-         if (MA_5[-2] < MA_5[-3] and MA_10[-2] < MA_10[-3] and MA_20[-2] < MA_20[-3] and MA_30[-2] < MA_30[-3]):
-              qushi_5_10_20_30 = "均线5、10、20、30齐降2"
-              if (MA_5[-3] < MA_5[-4] and MA_10[-3] < MA_10[-4] and MA_20[-3] < MA_20[-4] and MA_30[-3] < MA_30[-4]):
-                   qushi_5_10_20_30 = "均线5、10、20、30齐降3"
-                   if (MA_5[-4] < MA_5[-5] and MA_10[-4] < MA_10[-5] and MA_20[-4] < MA_20[-5] and MA_30[-4] < MA_30[-5]):
-                        qushi_5_10_20_30 = "均线5、10、20、30齐降4"
-                        if (MA_5[-5] < MA_5[-6] and MA_10[-5] < MA_10[-6] and MA_20[-5] < MA_20[-6] and MA_30[-5] < MA_30[-6]):
-                             qushi_5_10_20_30 = "均线5、10、20、30齐降5"
-                             if (MA_5[-6] < MA_5[-7] and MA_10[-6] < MA_10[-7] and MA_20[-6] < MA_20[-7] and MA_30[-6] < MA_30[-7]):
-                                  qushi_5_10_20_30 = "均线5、10、20、30齐降6"
-                                  if (MA_5[-7] < MA_5[-8] and MA_10[-7] < MA_10[-8] and MA_20[-7] < MA_20[-8] and MA_30[-7] < MA_30[-8]):
-                                       qushi_5_10_20_30 = "均线5、10、20、30齐降7"
-                                       if (MA_5[-8] < MA_5[-9] and MA_10[-8] < MA_10[-9] and MA_20[-8] < MA_20[-9] and MA_30[-8] < MA_30[-9]):
-                                            qushi_5_10_20_30 = "均线5、10、20、30齐降8"
-                                            if (MA_5[-9] < MA_5[-10] and MA_10[-9] < MA_10[-10] and MA_20[-9] < MA_20[-10] and MA_30[-9] < MA_30[-10]):
-                                                 qushi_5_10_20_30 = "均线5、10、20、30齐降9"
+          qushi_5_10_20_30 = "均线5、10、20、30齐降1"
+          if (MA_5[-2] < MA_5[-3] and MA_10[-2] < MA_10[-3] and MA_20[-2] < MA_20[-3] and MA_30[-2] < MA_30[-3]):
+               qushi_5_10_20_30 = "均线5、10、20、30齐降2"
+               if (MA_5[-3] < MA_5[-4] and MA_10[-3] < MA_10[-4] and MA_20[-3] < MA_20[-4] and MA_30[-3] < MA_30[-4]):
+                    qushi_5_10_20_30 = "均线5、10、20、30齐降3"
+                    if (MA_5[-4] < MA_5[-5] and MA_10[-4] < MA_10[-5] and MA_20[-4] < MA_20[-5] and MA_30[-4] < MA_30[
+                         -5]):
+                         qushi_5_10_20_30 = "均线5、10、20、30齐降4"
+                         if (MA_5[-5] < MA_5[-6] and MA_10[-5] < MA_10[-6] and MA_20[-5] < MA_20[-6] and MA_30[-5] <
+                                 MA_30[-6]):
+                              qushi_5_10_20_30 = "均线5、10、20、30齐降5"
+                              if (MA_5[-6] < MA_5[-7] and MA_10[-6] < MA_10[-7] and MA_20[-6] < MA_20[-7] and MA_30[
+                                   -6] < MA_30[-7]):
+                                   qushi_5_10_20_30 = "均线5、10、20、30齐降6"
+                                   if (MA_5[-7] < MA_5[-8] and MA_10[-7] < MA_10[-8] and MA_20[-7] < MA_20[-8] and
+                                           MA_30[-7] < MA_30[-8]):
+                                        qushi_5_10_20_30 = "均线5、10、20、30齐降7"
+                                        if (MA_5[-8] < MA_5[-9] and MA_10[-8] < MA_10[-9] and MA_20[-8] < MA_20[-9] and
+                                                MA_30[-8] < MA_30[-9]):
+                                             qushi_5_10_20_30 = "均线5、10、20、30齐降8"
+                                             if (MA_5[-9] < MA_5[-10] and MA_10[-9] < MA_10[-10] and MA_20[-9] < MA_20[
+                                                  -10] and MA_30[-9] < MA_30[-10]):
+                                                  qushi_5_10_20_30 = "均线5、10、20、30齐降9"
 
-     stock_data = {}
-     low_list = data_history.low.rolling(9).min()
-     low_list.fillna(value=data_history.low.expanding().min(), inplace=True)
-     high_list = data_history.high.rolling(9).max()
-     high_list.fillna(value=data_history.high.expanding().max(), inplace=True)
-     rsv = (doubleCloseArray - low_list) / (high_list - low_list) * 100
-     stock_data['KDJ_K'] = pd.DataFrame.ewm(rsv, com=2).mean()
-     stock_data['KDJ_D'] = pd.DataFrame.ewm(stock_data['KDJ_K'], com=2).mean()
-     stock_data['KDJ_J'] = 3 * stock_data['KDJ_K'] - 2 * stock_data['KDJ_D']
-     dddd = pd.DataFrame(stock_data)
-     KDJ_J_title = "KDJ_" + "%.2f" % dddd.KDJ_J[len(dddd) - 1] + " "
+     return MA20_titile, MA30_titile, MA60_titile, qushi_5_10_20_30
 
-     # macd 为快线 macdsignal为慢线，macdhist为柱体
-     macd, macdsignal, macdhist = ta.MACD(doubleCloseArray, fastperiod=12, slowperiod=26, signalperiod=9)
-     MACD_title=""
-     if (macdsignal[-1] > macdsignal[-2]):
-          MACD_title = "MACD慢线上行1"
-          if (macdsignal[-2] > macdsignal[-3]):
-               MACD_title = "MACD慢线上行2"
-               if (macdsignal[-3] > macdsignal[-4]):
-                    MACD_title = "MACD慢线上行3"
-                    if (macdsignal[-4] > macdsignal[-5]):
-                         MACD_title = "MACD慢线上行4"
-                         if (macdsignal[-5] > macdsignal[-6]):
-                              MACD_title = "MACD慢线上行5"
-                              if (macdsignal[-6] > macdsignal[-7]):
-                                   MACD_title = "MACD慢线上行6"
-                                   if (macdsignal[-7] > macdsignal[-8]):
-                                        MACD_title = "MACD慢线上行7"
-                                        if (macdsignal[-8] > macdsignal[-9]):
-                                             MACD_title = "MACD慢线上行8"
-                                             if (macdsignal[-9] > macdsignal[-10]):
-                                                  MACD_title = "MACD慢线上行9"
-                                                  if (macdsignal[-11] > macdsignal[-12]):
-                                                       MACD_title = "MACD慢线上行10"
+'''
+公共功能：指标
+'''
+def zhibiao(code, type):
+     data_history = ts.get_k_data(code, ktype=type)
 
-     upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray, timeperiod=20, nbdevup=2, nbdevdn=2,
-                                                        matype=0)
+     closeArray = num.array(data_history['close'])
+     highArray = num.array(data_history['high'])
+     lowArray = num.array(data_history['low'])
+     openArray = num.array(data_history['open'])
 
-     BULL_middleband = ""
-     if (middleband[-1] >= middleband[-2]):
-          BULL_middleband = "布林中线趋势上升"
-     else:
-          BULL_middleband = "布林中线趋势下降"
+     doubleCloseArray = num.asarray(closeArray, dtype='double')
+     doubleHighArray = num.asarray(highArray, dtype='double')
+     doubleLowArray = num.asarray(lowArray, dtype='double')
+     doubleOpenArray = num.asarray(openArray, dtype='double')
 
-     BULL_title = ""
-     if (closeArray[-1] > upperband[-1]):
-          BULL_title = "上穿布林线上沿"
-
-     if (lowArray[-1] < lowerband[-1] * 1.005):
-          BULL_title = "下穿布林线下沿"
-
-     # 返回20均线是否上传，30均线趋势, KDJ J 是否处于低位， MACD是否转折
-     # MA30_titile = MA30_titile + "%.2f" % MA_30[-1] + "_" + "%.2f" % MA_30[-2] + "_" + "%.2f" % MA_30[-3]
-     # BULL_title = BULL_title + "%.2f" % lowerband[-1] + "_" + "%.2f" % lowerband[-2] + "_" + "%.2f" % lowerband[-3]
+     # 股票当前价格
+     price =  "%.2f" % doubleCloseArray[-1] + "_" + "%.2f" % doubleCloseArray[-2] + "_" + "%.2f" % doubleCloseArray[-3]
+     # KDJ 指标
+     KDJ_J_title = KDJ_zhibiao(data_history, doubleCloseArray)
+     # MACD 指标
+     MACD_title = MACD_zhibiao(doubleCloseArray)
+     # BULL 指标
+     BULL_title, BULL_middleband = BULL_zhibiao(doubleCloseArray, closeArray, lowArray)
+     # 均线指标
+     MA20_titile, MA30_titile, MA60_titile, qushi_5_10_20_30 = junxian_zhibiao(doubleCloseArray, doubleOpenArray)
+     # 指标返回
      return price, MA20_titile, MA30_titile, MA60_titile, qushi_5_10_20_30, KDJ_J_title, MACD_title, BULL_title, BULL_middleband
 
 # 返回20均线是否上传，30均线趋势
-# MA20_titile, MA30_titile, KDJ_J_title, MACD_title, BULL_title = zhibiao('002010','D')
-# print(MA20_titile)
-# print(MA30_titile)
+# price, MA20_titile, MA30_titile, MA60_titile, qushi_5_10_20_30, KDJ_J_title, MACD_title, BULL_title, BULL_middleband = zhibiao('002010','D')
+# print(price)
 # print(KDJ_J_title)
 # print(MACD_title)
 # print(BULL_title)
+# print(BULL_middleband)
+# print(MA20_titile)
