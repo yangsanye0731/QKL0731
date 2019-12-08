e33b493f5ff27c9c5ad168ff7815051b92759add
yangsanye2019 <317491841@qq.com>
2019-12-08 14:21:50 +0800

    init

--- AGU_XUANGUBAO_DATA.py
+++ AGU_XUANGUBAO_DATA.py
@@ -31,8 +31,8 @@ def strategy():
         # 换手率
         turnover_rate = num.array(df['turnover_rate'])
         # 名称
-        time.sleep(30)
+        time.sleep(15)
         name = common.codeName(code)
         common_mysqlUtil.update_xuangubao(name, "%.2f" % (total_mv / 10000), "%.2f" % pe, "%.2f" % turnover_rate, code)
-        time.sleep(30)
+        time.sleep(15)
 strategy()
