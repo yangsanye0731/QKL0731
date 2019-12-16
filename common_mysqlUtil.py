f62af1f512ec6e04e0ae0d8793c80b3704a3d833
yangsanye2019 <317491841@qq.com>
2019-12-16 20:58:42 +0800

    init

--- common_mysqlUtil.py
+++ common_mysqlUtil.py
@@ -104,7 +104,13 @@ def insert_zhishu_record(code, name, fullName, plate, mark, type):
         price, MA20_titile, MA30_titile, MA60_titile, qushi_5_10_20_30, KDJ_J_title, MACD_title, BULL_title, BULL_middleband = common_zhibiao.zhibiao(code, 'D')
         price_60, MA20_titile_60, MA30_titile_60, MA60_titile_60, qushi_5_10_20_30_60, KDJ_J_title_60, MACD_title_60, BULL_title_60, BULL_middleband_60 = common_zhibiao.zhibiao(code, '60')
         price_30, MA20_titile_30, MA30_titile_30, MA60_titile_30, qushi_5_10_20_30_30, KDJ_J_title_30, MACD_title_30, BULL_title_30, BULL_middleband_30 = common_zhibiao.zhibiao(code, '30')
-        zhangdiefu = common.zhangdiefu(code)
+
+        data = select_xuangubao_one(code)
+        huanshoulv = ""
+        print(data)
+        if len(data) > 0:
+            huanshoulv = "@换手：" + data[0][4]
+        zhangdiefu = common.zhangdiefu(code) + huanshoulv
         print(fullName + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         title = fullName + zhangdiefu + " "
         content = fullName + zhangdiefu + qushi_5_10_20_30_30 + "<br>"
@@ -227,6 +233,22 @@ def select_xuangubao():
     cursor = db.cursor()
     # 使用 execute()  方法执行 SQL 查询
     sql = "SELECT `code`, `name`, `plate`, `mark` FROM `superman`.`AGU_Code` order by insert_time ASC"
+    print(sql)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return data
+
+def select_xuangubao_one(code):
+    userName = cf.get("MySql", "userName")
+    password = cf.get("MySql", "password")
+    # 打开数据库连接
+    db = pymysql.connect("localhost", userName, password, "superman")
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    sql = "SELECT `code`, `name`, `plate`, `mark`, `huanshoulv` FROM `superman`.`AGU_Code` WHERE `code` = " + code
     print(sql)
     cursor.execute(sql)
     data = cursor.fetchall()
