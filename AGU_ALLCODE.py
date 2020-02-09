057c851020643e852b801b8bdb6a038dc1842dc3
杨金鹏_18731 <18731@etransfar.com>
2020-02-09 15:34:29 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -56,7 +56,7 @@ common_mysqlUtil.insert_ZhiShuLog_record("======", "======", "ACD", "====", "===
 # time.sleep(0.5)
 # common.dingding_markdown_msg_2("触发孕线D策略完成(" + str_result + ")", "触发孕线D策略完成(" + str_result + ")")
 
-str_result = strategy('W', -1)
+str_result = strategy('W', -2)
 common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
 time.sleep(0.5)
 common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
