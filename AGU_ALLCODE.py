e7ee12c50d3fc621420be35f60c606f9550e2ada
杨金鹏_18731 <18731@etransfar.com>
2020-02-10 23:19:08 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -61,7 +61,7 @@ common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", 
 time.sleep(0.5)
 common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
 
-str_result = strategy('M', -2)
-common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+str_result = strategy('M', -1)
+common.dingding_markdown_msg_2("触发孕线M策略完成(" + str_result + ")", "触发孕线M策略完成(" + str_result + ")")
 time.sleep(0.5)
-common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+common.dingding_markdown_msg_2("触发孕线M策略完成(" + str_result + ")", "触发孕线M策略完成(" + str_result + ")")
