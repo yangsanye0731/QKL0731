6982b4d7cbfe01a821441ab4bb362d2155b4b03f
杨金鹏_18731 <18731@etransfar.com>
2020-02-10 15:20:51 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -56,7 +56,12 @@ common.dingding_markdown_msg_2("触发孕线D策略完成(" + str_result + ")", 
 time.sleep(0.5)
 common.dingding_markdown_msg_2("触发孕线D策略完成(" + str_result + ")", "触发孕线D策略完成(" + str_result + ")")
 
-# str_result = strategy('W', 0)
-# common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
-# time.sleep(0.5)
-# common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+str_result = strategy('W', 0)
+common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+time.sleep(0.5)
+common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+
+str_result = strategy('M', -1)
+common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
+time.sleep(0.5)
+common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
