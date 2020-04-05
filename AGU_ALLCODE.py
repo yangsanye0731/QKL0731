6d7a6296b0660b72b6535c7afdb636ff4bba97e1
杨金鹏_18731 <18731@etransfar.com>
2020-04-05 12:41:03 +0800

    init

--- AGU_ALLCODE.py
+++ AGU_ALLCODE.py
@@ -64,15 +64,17 @@ def strategy(zhouqi, n):
     return str(str_result)
 
 common_mysqlUtil.insert_ZhiShuLog_record("======", "======", "ACD", "====", "========", "============", "======", "")
-m = -5
+m = 0
 str_result = strategy('D', m)
 str_result = strategy('D', m-1)
 str_result = strategy('D', m-2)
 str_result = strategy('D', m-3)
+str_result = strategy('D', m-4)
 # common.dingding_markdown_msg_2("触发孕线D策略完成(" + str_result + ")", "触发孕线D策略完成(" + str_result + ")")
 # time.sleep(0.5)
 # common.dingding_markdown_msg_2("触发孕线D策略完成(" + str_result + ")", "触发孕线D策略完成(" + str_result + ")")
 
+str_result = strategy('W', 0)
 str_result = strategy('W', -1)
 print (dict)
 # common.dingding_markdown_msg_2("触发孕线W策略完成(" + str_result + ")", "触发孕线W策略完成(" + str_result + ")")
