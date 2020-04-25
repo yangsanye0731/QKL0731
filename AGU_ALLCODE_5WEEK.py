74f075a2d604499a7740047240d6c1283ced7cd9
杨金鹏_18731 <18731@etransfar.com>
2020-04-25 12:23:15 +0800

    init

--- AGU_ALLCODE_5WEEK.py
+++ AGU_ALLCODE_5WEEK.py
@@ -1,4 +1,4 @@
-#encoding=utf-8
+# encoding=utf-8
 import pandas as pd
 import time
 import numpy as num
@@ -15,7 +15,6 @@ def strategy(zhouqi):
     all_code_index_x = num.array(all_code_index)
 
     strResult = ""
-    strResult_2 = ""
     for codeItem in all_code_index_x:
         count = count + 1
         print(count)
@@ -33,28 +32,20 @@ def strategy(zhouqi):
 
             # 均线
             ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
-            eps, epsup, yingyeup, eps_2, epsup_2, yingyeup_2 = common.codeEPS(codeItem)
-            codeName = common.codeName(codeItem)
-            # print(codeItem)
-            # print(doubleCloseArray[-1])
-            # print(ma5[-1])
-            if (doubleHighArray[-1] > ma5[-1] and doubleOpenArray[-1] < ma5[-1] and epsup > 0 and yingyeup > 0):
-                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "======================================" + codeItem)
-                common_image.plt_image_kuaYueWeek5Line(codeItem, codeName, "W", "%.1f" % epsup, "%.1f" % yingyeup)
-                strResult += common.codeName(codeItem) + "跨越五周线" + "<br>"
 
-            if (ma5[-1] < ma5[-2] and ma5[-2] < ma5[-3] and ma5[-3] < ma5[-4] and epsup > 0 and yingyeup > 0 ):
-                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "======================================" + codeItem)
-                common_image.plt_image_lianXuXiaJiangWeek5Line(codeItem, codeName, "W", "%.1f" % epsup, "%.1f" % yingyeup)
-                strResult_2 += common.codeName(codeItem) + "5周线连续下降" + "<br>"
-
-            time.sleep(2)
+            # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
+            if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] and \
+                    ma5[-3] < ma5[-4]:
+                common_image.plt_image_tongyichutu(codeItem, "W", "跨越5周线", "跨越5周线")
 
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
-    return strResult, strResult_2
+    return strResult
 
-strMailResult_W, strResult_2 = strategy('W')
+strMailResult_W = strategy('W')
 sendMail(template1(strMailResult_W), "跨域5周线")
-time.sleep(10)
-sendMail(template1(strResult_2), "5周线连续下降")
+
+bp = ByPy()
+timeStr1 = time.strftime("%Y%m%d", time.localtime())
+bp.mkdir(remotepath=timeStr1)
+bp.upload(localpath="./images/" + timeStr1, remotepath=timeStr1)
