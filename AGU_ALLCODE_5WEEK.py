99a2ca036ce574f6d955b6dc1d28fa7f7ad6b759
杨金鹏_18731 <18731@etransfar.com>
2020-04-25 20:13:55 +0800

    init

--- AGU_ALLCODE_5WEEK.py
+++ AGU_ALLCODE_5WEEK.py
@@ -36,7 +36,7 @@ def strategy(zhouqi):
 
             # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
             if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] and \
-                    ma5[-3] < ma5[-4]:
+                    ma5[-3] < ma5[-4] and doubleCloseArray[-1] > doubleOpenArray[-1]:
                 common_image.plt_image_tongyichutu(codeItem, "W", "跨越5周线", "跨越5周线")
 
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
