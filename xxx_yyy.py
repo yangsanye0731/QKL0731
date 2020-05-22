bdc0caed4ad43cfe984bf76ecb2a5fb10b351800
杨金鹏_18731 <18731@etransfar.com>
2020-05-22 22:25:46 +0800

    init

--- xxx_yyy.py
+++ xxx_yyy.py
@@ -59,7 +59,7 @@ async def index(page, cookie1, url, codeName):
 
 async def main(url, codeName):
     print(datetime.datetime.now())
-    await asyncio.sleep(10 + random.randint(1, 50))
+    await asyncio.sleep(10 + random.randint(1, 10))
     print(datetime.datetime.now())
     js1 = '''() =>{
            Object.defineProperties(navigator,{
