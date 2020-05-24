f9a18b73a8e84d5b6935cf7fda12a3a3512ad962
杨金鹏_18731 <18731@etransfar.com>
2020-05-24 10:53:15 +0800

    init

--- AGU_ALLCODE_5WEEK.py
+++ AGU_ALLCODE_5WEEK.py
@@ -8,6 +8,7 @@ from email_util import *
 import common
 import common_image
 from bypy import ByPy
+import common_mysqlUtil
 
 def strategy(zhouqi):
     all_code = ts.get_stock_basics()
@@ -33,11 +34,17 @@ def strategy(zhouqi):
 
             # 均线
             ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
+            # ma20 = ta.SMA(doubleCloseArray, timeperiod=20)
+            ma30 = ta.SMA(doubleCloseArray, timeperiod=30)
+            ma60 = ta.SMA(doubleCloseArray, timeperiod=60)
 
             # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
             if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] and \
-                    ma5[-3] < ma5[-4] and doubleCloseArray[-1] > doubleOpenArray[-1]:
-                common_image.plt_image_tongyichutu(codeItem, "W", "跨越5周线", "跨越5周线")
+                    ma5[-3] < ma5[-4] and doubleCloseArray[-1] > doubleOpenArray[-1] and ma30[-1] > ma30[-2] and ma60[-1] > ma60[-2]:
+                print(codeItem + "=======================================================================================")
+                common.dingding_markdown_msg_2("触发20跨越5周线策略(" + codeItem + ")",
+                                               "触发20跨越5周线策略(" + codeItem + ")")
+                common_image.plt_image_tongyichutu(codeItem, "W", "20跨越5周线容大感光", "20跨越5周线容大感光")
 
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
@@ -50,3 +57,4 @@ bp = ByPy()
 timeStr1 = time.strftime("%Y%m%d", time.localtime())
 bp.mkdir(remotepath=timeStr1)
 bp.upload(localpath="./images/" + timeStr1, remotepath=timeStr1)
+common.dingding_markdown_msg_2('触发20跨越5周线执行完成', '触发20跨越5周线执行完成')
