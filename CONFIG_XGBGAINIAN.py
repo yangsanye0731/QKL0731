be936d29939b63b8648068e8f64b4beba5d8ba52
杨金鹏_18731 <18731@etransfar.com>
2020-05-24 22:17:26 +0800

    init

--- CONFIG_XGBGAINIAN.py
+++ CONFIG_XGBGAINIAN.py
@@ -30,7 +30,7 @@ async def index(page, cookie1, url, codeName):
     try:
         for cookie in cookie1:
             await page.setCookie(cookie)
-        await page.goto(url)
+        await page.goto(url, options={"timeout":10000})
         data_content = await page.xpath('//pre')
         # print(await (await data_content[0].getProperty("textContent")).jsonValue())
         json_list = json.loads(await (await data_content[0].getProperty("textContent")).jsonValue())
@@ -45,7 +45,6 @@ async def index(page, cookie1, url, codeName):
 
 async def main(url1, codeName):
     print(datetime.datetime.now())
-    await asyncio.sleep(5 + random.randint(1, 100))
     print(datetime.datetime.now())
     js1 = '''() =>{
            Object.defineProperties(navigator,{
@@ -72,6 +71,7 @@ async def main(url1, codeName):
     await save_cookie(cookies2)
     cookie = await load_cookie()
     result = await index(page, cookie, url1, codeName)
+    await browser.close()
     return result
 
 all_code = ts.get_stock_basics()
@@ -87,16 +87,16 @@ for codeItem in all_code_index_x:
         data = common_mysqlUtil.select_all_code_one(code)
         if len(data) > 0:
             plate = data[0][2]
-            if(len(plate)>1):
+            if(len(plate)>0):
                 continue
 
-        if codeItem.startswith('6'):
-            codeItem = codeItem + '.SS'
-        if codeItem.startswith('0'):
-            codeItem = codeItem + '.SZ'
-        if codeItem.startswith('3'):
-            codeItem = codeItem + '.SZ'
-        asyncio.get_event_loop().run_until_complete(main('https://flash-api.xuangubao.cn/api/stage2/plates_by_any_stock?symbol=' + codeItem + '&fields=core_avg_pcp,plate_name',
-            code))
+            if codeItem.startswith('6'):
+                codeItem = codeItem + '.SS'
+            if codeItem.startswith('0'):
+                codeItem = codeItem + '.SZ'
+            if codeItem.startswith('3'):
+                codeItem = codeItem + '.SZ'
+            asyncio.get_event_loop().run_until_complete(main('https://flash-api.xuangubao.cn/api/stage2/plates_by_any_stock?symbol=' + codeItem + '&fields=core_avg_pcp,plate_name',
+                code))
     except (IOError, TypeError, NameError, IndexError, TimeoutError, Exception) as e:
         print(e)
