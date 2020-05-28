217c8c4ab34fb939a7813dd3d127ac78bc893711
杨金鹏_18731 <18731@etransfar.com>
2020-05-28 20:10:32 +0800

    init

--- ZHISHU_5WEEK.py
+++ ZHISHU_5WEEK.py
@@ -32,26 +32,53 @@ def strategy(zhouqi, n):
         codeItem = key
         count = count + 1
         print(count)
-        data_history = ts.get_k_data(codeItem, ktype=zhouqi)
+        data_history_W = ts.get_k_data(codeItem, ktype='W')
+        data_history_M = ts.get_k_data(codeItem, ktype='M')
+        data_history_D = ts.get_k_data(codeItem, ktype='D')
 
         try:
-            closeArray = num.array(data_history['close'])
-            doubleCloseArray = num.asarray(closeArray, dtype='double')
-
-            highArray = num.array(data_history['high'])
-            doubleHighArray = num.asarray(highArray, dtype='double')
-
-            openArray = num.array(data_history['open'])
-            doubleOpenArray = num.asarray(openArray, dtype='double')
+            closeArray_W = num.array(data_history_W['close'])
+            doubleCloseArray_W = num.asarray(closeArray_W, dtype='double')
+            highArray_W = num.array(data_history_W['high'])
+            doubleHighArray_W = num.asarray(highArray_W, dtype='double')
+            openArray_W = num.array(data_history_W['open'])
+            doubleOpenArray_W = num.asarray(openArray_W, dtype='double')
 
             # 均线
-            ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
+            ma5 = ta.SMA(doubleCloseArray_W, timeperiod=5)
 
             # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
-            if doubleHighArray[n-1] > ma5[n-1] > doubleOpenArray[n-1] and ma5[n-2] < ma5[n-3] and \
-                    ma5[n-3] < ma5[n-4] and doubleCloseArray[n-1] > doubleOpenArray[n-1]:
+            if doubleHighArray_W[n-1] > ma5[n-1] > doubleOpenArray_W[n-1] and ma5[n-2] < ma5[n-3] and \
+                    ma5[n-3] < ma5[n-4] and doubleCloseArray_W[n-1] > doubleOpenArray_W[n-1]:
                 print(value)
-                common_image.plt_image_tongyichutu_zhishu(codeItem, value, "W", "【02国内指数】跨越5周线", "【02国内指数】跨越5周线")
+                common_image.plt_image_tongyichutu_zhishu(codeItem, value, "W", "【02国内指数】5周线上穿", "【02国内指数】5周线上穿")
+
+            closeArray_M = num.array(data_history_M['close'])
+            doubleCloseArray_M = num.asarray(closeArray_M, dtype='double')
+            lowArray_M = num.array(data_history_M['low'])
+            doubleLowArray_M = num.asarray(lowArray_M, dtype='double')
+            closeArray_D = num.array(data_history_D['close'])
+            doubleCloseArray_D = num.asarray(closeArray_D, dtype='double')
+            lowArray_D = num.array(data_history_D['low'])
+            doubleLowArray_D = num.asarray(lowArray_D, dtype='double')
+
+
+            param_m1 = 11
+            param_m2 = 9
+            param_n = 10
+            sma_n = ta.SMA(closeArray_M, param_n)
+            upper = (1 + param_m1 / 100) * sma_n
+            lower = (1 - param_m2 / 100) * sma_n
+            ene = (upper + lower) / 2
+            upper = upper.round(2)
+            ene = ene.round(2)
+            lower = lower.round(2)
+
+            if (ene[-1] > ene[-2] and doubleLowArray_M[-1] < ene[-1]):
+                upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray_D, timeperiod=20, nbdevup=2, nbdevdn=2,
+                                                             matype=0)
+                if doubleLowArray_D[-1] < lowerband[-1]:
+                    common_image.plt_image_tongyichutu_zhishu(codeItem, value, "W", "【02国内指数】ENE月线升势，布林日线下穿", "【02国内指数】ENE月线升势，布林日线下穿")
 
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
@@ -62,4 +89,4 @@ bp = ByPy()
 timeStr1 = time.strftime("%Y%m%d", time.localtime())
 bp.mkdir(remotepath=timeStr1)
 bp.upload(localpath="./images/" + timeStr1, remotepath=timeStr1)
-common.dingding_markdown_msg_2('触发【02国内指数】跨越5周线执行完成', '触发【02国内指数】跨越5周线执行完成')
+common.dingding_markdown_msg_2('触发【02国内指数】执行完成', '触发【02国内指数】执行完成')
