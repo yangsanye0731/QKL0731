5e02e08f0c5591d7458f11d2a85313e51478027a
杨金鹏_18731 <18731@etransfar.com>
2020-08-30 12:59:43 +0800

    init

--- report.py
+++ report.py
@@ -26,8 +26,7 @@ def code_strategy(codeItem, field_name, width):
     all_code_index_x = [codeItem]
     myimage = None
     image_path = None
-    sign_result = "信号："
-    strResult = ""
+    sign = "信号："
     for codeItem in all_code_index_x:
         time.sleep(1)
         count = count + 1
@@ -46,17 +45,15 @@ def code_strategy(codeItem, field_name, width):
             openArray = num.array(data_history['open'])
             doubleOpenArray = num.asarray(openArray, dtype='double')
 
-            lowArray = num.array(data_history['low'])
-            doubleLowArray = num.asarray(lowArray, dtype='double')
+            # lowArray = num.array(data_history['low'])
+            # doubleLowArray = num.asarray(lowArray, dtype='double')
 
             # 均线
             ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
             ma60 = ta.SMA(doubleCloseArray, timeperiod=60)
-
-            data = common_mysqlUtil.select_all_code_one(codeItem)
-            if len(data) > 0:
-                mingcheng = data[0][1]
-            image_path = common_image.plt_image_tongyichutu_3(codeItem, "W", "【04每日报告】全部",
+            image_path = common_image.plt_image_tongyichutu_3(codeItem,
+                                                              "W",
+                                                              "【04每日报告】全部",
                                                               "【04每日报告】全部")
             myimage = InlineImage(tpl, image_path, width=Mm(width))
             context[field_name] = myimage
@@ -64,18 +61,18 @@ def code_strategy(codeItem, field_name, width):
             # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
             if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] < ma5[-4] \
                     and doubleCloseArray[-1] > doubleOpenArray[-1] and ma60[-1] > ma60[-2]:
-                data = common_mysqlUtil.select_all_code_one(codeItem)
-                if len(data) > 0:
-                    mingcheng = data[0][1]
-                image_path = common_image.plt_image_tongyichutu_3(codeItem, "W", "【04每日报告】跨越5周线容大感光,主力持仓突增", "【04每日报告】跨越5周线容大感光,主力持仓突增")
-                myimage=InlineImage(tpl, image_path, width=Mm(width))
-                sign_result = sign_result + "触发跨越5周线容大感光,主力持仓突增;  "
+                image_path = common_image.plt_image_tongyichutu_3(codeItem,
+                                                                  "W",
+                                                                  "【04每日报告】跨越5周线容大感光,主力持仓突增",
+                                                                  "【04每日报告】跨越5周线容大感光,主力持仓突增")
+                myimage = InlineImage(tpl, image_path, width=Mm(width))
+                sign = sign + "触发跨越5周线容大感光,主力持仓突增;  "
                 context[field_name] = myimage
 
             closeArray_M = num.array(data_history_M['close'])
-            doubleCloseArray_M = num.asarray(closeArray_M, dtype='double')
-            lowArray_M = num.array(data_history_M['low'])
-            doubleLowArray_M = num.asarray(lowArray_M, dtype='double')
+            # doubleCloseArray_M = num.asarray(closeArray_M, dtype='double')
+            # lowArray_M = num.array(data_history_M['low'])
+            # doubleLowArray_M = num.asarray(lowArray_M, dtype='double')
             closeArray_D = num.array(data_history_D['close'])
             doubleCloseArray_D = num.asarray(closeArray_D, dtype='double')
             lowArray_D = num.array(data_history_D['low'])
@@ -88,15 +85,18 @@ def code_strategy(codeItem, field_name, width):
             upper = (1 + param_m1 / 100) * sma_n
             lower = (1 - param_m2 / 100) * sma_n
             ene = (upper + lower) / 2
-            upper = upper.round(2)
+            # upper = upper.round(2)
             ene = ene.round(2)
-            lower = lower.round(2)
+            # lower = lower.round(2)
 
             if ene[-1] > ene[-2]:
                 upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray_D, timeperiod=20, nbdevup=2, nbdevdn=2,
                                                              matype=0)
                 if doubleLowArray_D[-1] < lowerband[-1] * 1.008:
-                    image_path = common_image.plt_image_tongyichutu_3(codeItem, "W", "【04每日报告】ENE月线升势，布林日线下穿", "【04每日报告】ENE月线升势，布林日线下穿")
+                    image_path = common_image.plt_image_tongyichutu_3(codeItem,
+                                                                      "W",
+                                                                      "【04每日报告】ENE月线升势，布林日线下穿",
+                                                                      "【04每日报告】ENE月线升势，布林日线下穿")
                     myimage = InlineImage(tpl, image_path, width=Mm(width))
                     context[field_name] = myimage
 
@@ -104,19 +104,18 @@ def code_strategy(codeItem, field_name, width):
             upper_w = (1 + param_m1 / 100) * sma_n_w
             lower_w = (1 - param_m2 / 100) * sma_n_w
             ene_w = (upper_w + lower_w) / 2
-            upper_w = upper_w.round(2)
+            # upper_w = upper_w.round(2)
             ene_w = ene_w.round(2)
-            lower_w = lower_w.round(2)
+            # lower_w = lower_w.round(2)
 
             print("=====================================================" + codeItem)
             print(closeArray[-1])
             print(ene_w[-1])
             # if closeArray[-1] < ene_w[-1] :
-                # sign_result = sign_result + "触发当前价格在ENE周线中线下方；"
-
+                # sign = sign + "触发当前价格在ENE周线中线下方；"
         except (IOError, TypeError, NameError, IndexError, Exception) as e:
             print(e)
-    return myimage, sign_result, image_path
+    return myimage, sign, image_path
 
 # 返回时间所对应的星期值
 def get_week_day(date):
@@ -160,7 +159,7 @@ context['gainian4'] = gainian4
 gegu_list = []
 for i in range(gegu_count):
     gegu = cf.get("script", "gegu" + str(i))
-    image_path, sign_result, image_lujing = code_strategy(gegu.split('|')[1], "codeItemXXX", 135)
+    image_path_result, sign_result, image_lujing = code_strategy(gegu.split('|')[1], "codeItemXXX", 135)
     image_url = "http://47.240.11.144/" + image_lujing[9:]
     print(image_url)
     if "触发" in sign_result:
@@ -172,7 +171,7 @@ for i in range(gegu_count):
     rt1 = RichText('')
     rt1.add(sign_result, color='#ff0000', bold=True)
     gegu_dict = {'date': gegu.split('|')[0], 'title': gegu.split('|')[2], 'mark': gegu.split('|')[3],
-                 'qita': rt1, 'image_path': image_path}
+                 'qita': rt1, 'image_path': image_path_result}
     gegu_list.append(gegu_dict)
 context['gegu_list'] = gegu_list
 
@@ -272,7 +271,7 @@ genzong_list = []
 for i in range(gengong_count):
     try:
         genzong = cf.get("script", "genzong" + str(i))
-        image_path, sign_result, image_lujing = code_strategy(genzong.split('|')[1], "codeItemXXX", 120)
+        image_path_result, sign_result, image_lujing = code_strategy(genzong.split('|')[1], "codeItemXXX", 120)
         image_url = "http://47.240.11.144/" + image_lujing[9:]
         print(image_url)
         if "触发" in sign_result:
@@ -286,7 +285,7 @@ for i in range(gengong_count):
         rt1 = RichText('')
         rt1.add(sign_result, color='#ff0000', bold=True)
         gezong_dict = {'date': genzong.split('|')[0], 'title': genzong.split('|')[2] + '【' + sign_result + '】',
-                       'mark': '', 'qita': rt1, 'image_path': image_path}
+                       'mark': '', 'qita': rt1, 'image_path': image_path_result}
         genzong_list.append(gezong_dict)
         time.sleep(10)
     except (IOError, TypeError, NameError, IndexError, Exception) as e:
