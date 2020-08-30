a0d2e921b78b74c65d6a18ea0f41a1681ecfc61c
杨金鹏_18731 <18731@etransfar.com>
2020-08-30 20:38:44 +0800

    init

--- basic/CONFIG_XGBGAINIAN.py
+++ basic/CONFIG_XGBGAINIAN.py
@@ -8,6 +8,11 @@ from pyppeteer import launch
 
 #######################################################################################################################
 ################################################################################################配置程序应用所需要环境PATH
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 import common
 import common_mysqlUtil
 
