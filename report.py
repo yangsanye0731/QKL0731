ee9a4df58b77b4452914d389de413f2d6e7b1f18
杨金鹏_18731 <18731@etransfar.com>
2020-08-30 11:27:55 +0800

    init

--- report.py
+++ report.py
@@ -61,8 +61,8 @@ def code_strategy(codeItem, field_name, width):
             context[field_name] = myimage
 
             # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
-            if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] and \
-                    ma5[-3] < ma5[-4] and doubleCloseArray[-1] > doubleOpenArray[-1] and ma60[-1] > ma60[-2]:
+            if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] < ma5[-4] \
+                    and doubleCloseArray[-1] > doubleOpenArray[-1] and ma60[-1] > ma60[-2]:
                 data = common_mysqlUtil.select_all_code_one(codeItem)
                 if len(data) > 0:
                     mingcheng = data[0][1]
@@ -329,6 +329,7 @@ tpl.render(context)
 timeTitle = time.strftime("%Y%m%d", time.localtime())
 tpl.save('./report/每日报告_2020.docx')
 tpl.save('./report/每日报告_' + timeTitle + '.docx')
+
 #######################################################################################################################
 ################################################################################################################同步数据
 bp = ByPy()
