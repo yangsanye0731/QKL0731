252b39d1b61c96c6a1c84793ff80cb06506b4ed7
杨金鹏_18731 <18731@etransfar.com>
2020-09-05 16:30:39 +0800

    init

--- basic/CONFIG_XGB_HUANSHOU.py
+++ basic/CONFIG_XGB_HUANSHOU.py
@@ -1,17 +1,12 @@
 import asyncio
-from pyppeteer import launch
 import datetime
-import time
-from asyncio import sleep
 import json
-import pandas as pd
 import random
-import common
-import common_image
-from bypy import ByPy
-import talib as ta
+
 import numpy as num
 import tushare as ts
+from pyppeteer import launch
+
 import common_mysqlUtil
 
 async def save_cookie(cookie):
@@ -34,13 +29,13 @@ async def index(page, cookie1, url, codeName):
         data_content = await page.xpath('//pre')
         # print(await (await data_content[0].getProperty("textContent")).jsonValue())
         json_list = json.loads(await (await data_content[0].getProperty("textContent")).jsonValue())
-        # print(json_list.get('data').get('snapshot').items())
+        print(json_list.get('data').get('snapshot').items())
         print("==================================================================================")
         # common_mysqlUtil.update_all_code_plate(codeName, str(json.dumps(json_list.get('data'), ensure_ascii=False)))
         items = json_list.get('data').get('snapshot').items()
         for key, value in items:
-            print(value[1])
-            common_mysqlUtil.update_all_code_huanshoulv(codeName, "%.2f" % value[1])
+            print(value[11])
+            common_mysqlUtil.update_all_code_huanshoulv(codeName, "%.2f" % value[11])
     except (IOError, TypeError, NameError, IndexError, TimeoutError, Exception) as e:
         print(e)
     return result
@@ -65,7 +60,8 @@ async def main(url1, codeName):
 
     browser = await launch(headless=True, args=['--no-sandbox'])
     page = await browser.newPage()
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36')
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) '
+                            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36')
     await page.goto("https://www.xuangubao.cn/")
     await page.evaluate(js1)
     # await page.evaluate(js2)
@@ -95,7 +91,10 @@ for codeItem in all_code_index_x:
             codeItem = codeItem + '.SZ'
         if codeItem.startswith('3'):
             codeItem = codeItem + '.SZ'
-        url = "https://api-ddc-wscn.xuangubao.cn/market/real?fields=prod_name,turnover_ratio&prod_code=" + codeItem
+        url = "https://api-ddc-wscn.xuangubao.cn/market/real?fields=prod_name,trade_status,update_time,last_px," \
+              "px_change,px_change_rate,preclose_px,open_px,high_px,low_px,amplitude,turnover_ratio,pe_rate,dyn_pe," \
+              "dyn_pb_rate,market_value,circulation_value,turnover_volume,turnover_value,hq_type_code," \
+              "securities_type,volume_ratio,circulation_shares,total_shares,bps&prod_code=" + codeItem
         asyncio.get_event_loop().run_until_complete(main(url, code))
     except (IOError, TypeError, NameError, IndexError, TimeoutError, Exception) as e:
         print(e)
