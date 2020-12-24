4652c9260730f3ddce820e98d8d24e72c0c68f54
杨金鹏_18731 <18731@etransfar.com>
2020-12-24 19:47:09 +0800

    init

--- AGU_SHUJU.py
+++ AGU_SHUJU.py
@@ -77,7 +77,7 @@ def strategy(type):
         send_image(code='399300', name='深证指数')
         # send_image(code='300322', name='【跨越5月线】硕贝德', only_qushi_image=False)
         send_image(code='603363', name='【猪周期，跨越5周线，主力持仓增长】傲农生物', only_qushi_image=False,
-                   item_image_url='http://47.240.11.144/software/QKL0713/resource/images/ANSW.png')
+                   item_image_url='http://47.240.11.144/software/QKL0731/resource/images/ANSW.png')
         send_image(code='002548', name='【猪周期，跨越5周线】金新农', only_qushi_image=False)
         send_image(code='300498', name='【大市值猪肉股】温氏股份', only_qushi_image=False)
 
