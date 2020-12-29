619beaac7e0a09f478d25579c1bad73f9e04fa44
杨金鹏_18731 <18731@etransfar.com>
2020-12-29 10:20:30 +0800

    init

--- common_mysqlUtil.py
+++ common_mysqlUtil.py
@@ -326,6 +326,21 @@ def select_all_code_one(code):
     db.close()
     return data
 
+def select_all_code_by_gainian(gainian_name):
+    userName = cf.get("MySql", "userName")
+    password = cf.get("MySql", "password")
+    # 打开数据库连接
+    db = pymysql.connect("localhost", userName, password, "superman")
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    sql = "SELECT `code`, `name`, `plate`, `mark`, `huanshoulv`, `epsup`, `yingyeup` FROM `superman`.`AGU_All_Code` WHERE `plate` like %" + gainian_name + "%"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return data
+
 def update_xuangubao(name, shizhi, shiyinglv, huanshoulv, code, epsup, yingyeup):
     sql = ""
     if (len(name) == 0):
