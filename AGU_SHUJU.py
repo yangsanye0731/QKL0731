6dd606d7e477ed054af134ebf435185e5a793d76
杨金鹏_18731 <18731@etransfar.com>
2021-01-02 21:42:00 +0800

    init

--- AGU_SHUJU.py
+++ AGU_SHUJU.py
@@ -4,13 +4,13 @@ import common_mysqlUtil
 import datalab.s1_yueDuZeShi.yueDuZeShi as ydzs
 
 
-def send_image(code, name, only_qushi_image=False, item_image_url=''):
+def send_image(code, name, only_qushi_image=False, item_image_url='', message=''):
     zhangdiefu, price = common.zhangdiefu_and_price(code)
     if only_qushi_image:
         image_path = common_image.plt_image_geGuZhiBiao(code, name)
         image_url = "http://47.240.11.144/" + image_path[6:]
         print(image_url)
-        common.dingding_markdown_msg_2("触发" + name + zhangdiefu, "触发" + name + zhangdiefu + " 价格：" + price
+        common.dingding_markdown_msg_2("触发" + name + zhangdiefu, "触发" + name + zhangdiefu + " 价格：" + price + message
                                        + " ![screenshot]("
                                        + image_url + ")")
     else:
@@ -20,7 +20,7 @@ def send_image(code, name, only_qushi_image=False, item_image_url=''):
         image_path = common_image.plt_image_geGuZhiBiao(code, name)
         image_url = "http://47.240.11.144/" + image_path[6:]
         print(image_url)
-        common.dingding_markdown_msg_2("触发" + name + zhangdiefu, "触发" + name + zhangdiefu + " 价格：" + price
+        common.dingding_markdown_msg_2("触发" + name + zhangdiefu, "触发" + name + zhangdiefu + " 价格：" + price + message
                                        + " ![screenshot](" + image_url + ")"
                                        + " ![screenshot](" + image_url_ydzs + ")"
                                        + " ![screenshot](" + item_image_url + ")")
@@ -51,7 +51,8 @@ def strategy(type):
     print("总数618：" + str(int(data1[0][0] * 0.6)))
 
     flag = False
-    if (data1[0][5] >= 30 or data1[0][6] >= 30):
+    str_message = ''
+    if data1[0][5] >= 30 or data1[0][6] >= 30:
         flag = True
 
         # sendMail("30MIN上升数，下降数达到一半", "30MIN上升数，下降数达到一半")
@@ -59,27 +60,29 @@ def strategy(type):
                                        + "，下降数:" + str(data1[0][6]) + "达到一半",
                                        "【重要通知，规范流程】" + type + "30MIN上升数:" + str(data1[0][5])
                                        + "，下降数:" + str(data1[0][6]) + "达到一半")
+        str_message = str_message + "@@" + type + "30MIN上升数:" + str(data1[0][5]) + "，下降数:" + str(data1[0][6]) + "达到一半"
 
-    if (data1[0][3] >= 30 or data1[0][4] >= 30):
+    if data1[0][3] >= 30 or data1[0][4] >= 30:
         flag = True
         # sendMail("60MIN上升数，下降数达到一半", "60MIN上升数，下降数达到一半")
         common.dingding_markdown_msg_2("【重要通知，规范流程】" + type + "60MIN上升数:" + str(data1[0][3])
                                        + "，下降数:" + str(data1[0][4]) + "达到一半",
                                        "【重要通知，规范流程】" + type + "60MIN上升数:" + str(data1[0][3])
                                        + "，下降数:" + str(data1[0][4]) + "达到一半")
+        str_message = str_message + "@@" + type + "60MIN上升数:" + str(data1[0][3]) + "，下降数:" + str(data1[0][4]) + "达到一半"
 
     ####################################################################################################################
     ##########################################################################################################指数指标图片
     flag = True
     if flag:
-        send_image(code='sh', name='上证指数')
-        send_image(code='cyb', name='创业板指数')
-        send_image(code='399300', name='深证指数')
+        send_image(code='sh', name='上证指数', message=str_message)
+        send_image(code='cyb', name='创业板指数', message=str_message)
+        send_image(code='399300', name='深证指数', message=str_message)
         # send_image(code='300322', name='【跨越5月线】硕贝德', only_qushi_image=False)
         send_image(code='603363', name='【猪周期，跨越5周线，主力持仓增长】傲农生物', only_qushi_image=False,
-                   item_image_url='http://47.240.11.144/software/QKL0731/resource/images/ANSW1.png')
+                   item_image_url='http://47.240.11.144/software/QKL0731/resource/images/ANSW1.png', message=str_message)
         # send_image(code='002548', name='【猪周期，跨越5周线】金新农', only_qushi_image=False)
-        send_image(code='600332', name='【仿制药】白云山', only_qushi_image=False)
+        send_image(code='600332', name='【仿制药】白云山', only_qushi_image=False, message=str_message)
 
 
 # strategy("ZXG")
