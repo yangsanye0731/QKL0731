e57d94c002328d9676b5998c6fb2a8018f4de04c
杨金鹏_18731 <18731@etransfar.com>
2021-02-15 21:28:38 +0800

    init

--- common_image.py
+++ common_image.py
@@ -1174,15 +1174,15 @@ def plt_image_geGuZhiBiao_array(code, fullName, code2, fullName2, code3, fullNam
     turnover_rate = "%.2f" % turnover_rate
 
     myfont = matplotlib.font_manager.FontProperties(fname=rootPath + os.sep + "simsun.ttc", size="10")
-
+    myfont2 = matplotlib.font_manager.FontProperties(fname=rootPath + os.sep + "simsun.ttc", size="14")
     fig = plt.figure(figsize=(20, 16))
-    fig.suptitle("步骤1：上证、创业板等指数30分钟、60分钟KDJ\n"
-                 "步骤二：查看板块主力资金进入大于10%，且处于较低位置\n"
-                 "步骤三：资金板块内分布不集中与个股，较均匀分不到不同股票上\n"
-                 "步骤三：寻找\n"
-                 "步骤四：买入\n"
-                 "步骤五：卖出",
-                 fontproperties=myfont)
+    fig.suptitle("步骤一：查看上证、深证、创业板指数30分钟、60分钟KDJ走势，30分钟快线处于0以下，慢线处于30以下，KDJ反转；\n"
+                 "步骤二：查看短线王中各个板块主力资金进入情况，筛选大于10%，且价格处于较低位置的板块\n"
+                 "步骤三：查看板块中资金分布情况，筛选均匀分不到不同的龙头股票上\n"
+                 "步骤四：查看股票30分钟、15分钟内的主力资金进场情况\n"
+                 "步骤五：买入\n"
+                 "步骤六：卖出，KDJ第一次反转立即卖出，不管盈亏，卖出",
+                 fontproperties=myfont2)
     # fig.suptitle(codeName, fontproperties=myfont_title)
     # 1*1 的第一个图表
     ax_kdj_30 = fig.add_subplot(5, 4, 1)
