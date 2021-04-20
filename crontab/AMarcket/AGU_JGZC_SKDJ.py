c474e3de1b2d7f0eac95341d48395b50487cb455
杨金鹏_18731 <18731@etransfar.com>
2021-04-20 15:20:11 +0800

    init

--- crontab/AMarcket/AGU_JGZC_SKDJ.py
+++ crontab/AMarcket/AGU_JGZC_SKDJ.py
@@ -16,68 +16,76 @@ import common
 import common_image
 import common_zhibiao
 
+count = 0
+count_b = 0
+time_str = time.strftime("%Y%m%d", time.localtime())
+
+
+#######################################################################################################################
+####################################################################################################################EXE
+def exe(one_column_data, zhouqi, endstr, fo):
+    print('{:0>6d}'.format(one_column_data[1].value))
+    print(one_column_data[2].value)
+    codeItem = '{:0>6d}'.format(one_column_data[1].value)
+    time.sleep(0.01)
+    # count = count + 1
+    # print(count)
+
+    try:
+        data_history = ts.get_hist_data(codeItem, ktype=zhouqi, end=endstr)
+        data_history = data_history.iloc[::-1]
+        closeArray = num.array(data_history['close'])
+        doubleCloseArray = num.asarray(closeArray, dtype='double')
+
+        highArray = num.array(data_history['high'])
+        doubleHighArray = num.asarray(highArray, dtype='double')
+
+        openArray = num.array(data_history['open'])
+        doubleOpenArray = num.asarray(openArray, dtype='double')
+
+        # print(data_history)
+        k0, d0 = common_zhibiao.SKDJ_zhibiao(data_history, doubleCloseArray)
+
+        if k0[-1] < 55 and k0[-2] < d0[-2] and k0[-1] > d0[-1]:
+            print(codeItem + "========================================")
+            print(k0[-1])
+            print(d0[-1])
+            MA_20 = ta.SMA(doubleCloseArray, timeperiod=20)
+            if MA_20[-1] > MA_20[-2]:
+                fo.write(codeItem + "\n")
+    except (IOError, TypeError, NameError, IndexError, Exception) as e:
+        print(e)
 
 #######################################################################################################################
 ###########################################################################################################跨域5月线策略
 def strategy(zhouqi, endstr):
     # 局部变量初始化
-    count = 0
-    count_b = 0
-
+    fo = open("SKDJ_JGZC_" + zhouqi + "_" + endstr + ".txt", "w")
     wb = openpyxl.load_workbook('JGZC.xlsx')
     # 获取所有工作表名
     names = wb.sheetnames
     # wb.get_sheet_by_name(name) 已经废弃,使用wb[name] 获取指定工作表
     sheet = wb[names[0]]
+    sheet1 = wb[names[1]]
+    sheet2 = wb[names[2]]
+    sheet3 = wb[names[3]]
 
-    time_str = time.strftime("%Y%m%d", time.localtime())
-    fo = open("SKDJ_JGZC_" + zhouqi + "_" + endstr + ".txt", "w")
     for one_column_data in sheet.iter_rows():
-        print('{:0>6d}'.format(one_column_data[1].value))
-        print(one_column_data[2].value)
-        codeItem = '{:0>6d}'.format(one_column_data[1].value)
-        time.sleep(0.01)
-        count = count + 1
-        print(count)
+        exe(one_column_data,zhouqi,endstr, fo)
+    fo.close()
 
-        try:
-            data_history = ts.get_hist_data(codeItem, ktype=zhouqi, end=endstr)
-            data_history = data_history.iloc[::-1]
-            closeArray = num.array(data_history['close'])
-            doubleCloseArray = num.asarray(closeArray, dtype='double')
+    for one_column_data in sheet1.iter_rows():
+        exe(one_column_data,zhouqi,endstr, fo)
+    fo.close()
 
-            highArray = num.array(data_history['high'])
-            doubleHighArray = num.asarray(highArray, dtype='double')
+    for one_column_data in sheet2.iter_rows():
+        exe(one_column_data,zhouqi,endstr, fo)
+    fo.close()
 
-            openArray = num.array(data_history['open'])
-            doubleOpenArray = num.asarray(openArray, dtype='double')
+    for one_column_data in sheet3.iter_rows():
+        exe(one_column_data,zhouqi,endstr, fo)
+    fo.close()
 
-            # print(data_history)
-            k0,d0 = common_zhibiao.SKDJ_zhibiao(data_history, doubleCloseArray)
-
-            if k0[-1] < 55 and k0[-2] < d0[-2] and k0[-1] > d0[-1]:
-                print(codeItem + "========================================")
-                print(k0[-1])
-                print(d0[-1])
-                MA_20 = ta.SMA(doubleCloseArray, timeperiod=20)
-                if MA_20[-1] > MA_20[-2]:
-                    fo.write(codeItem + "\n")
-
-
-            # # 均线
-            # ma5 = ta.SMA(doubleCloseArray, timeperiod=5)
-            # ma60 = ta.SMA(doubleCloseArray, timeperiod=60)
-            #
-            # # 跨越5周线, 最高点大于5周线, 开点小于5周线, 前两周五周线处于下降阶段
-            # if doubleHighArray[-1] > ma5[-1] > doubleOpenArray[-1] and ma5[-2] < ma5[-3] < ma5[-4] \
-            #         and doubleCloseArray[-1] > doubleOpenArray[-1] and ma60[-1] > ma60[-2]:
-            #     common_image.plt_image_tongyichutu_2(codeItem,
-            #                                          "M",
-            #                                          "【03全部代码】跨越5月线",
-            #                                          "【03全部代码】跨越5月线")
-            #     count_b = count_b + 1
-        except (IOError, TypeError, NameError, IndexError, Exception) as e:
-            print(e)
     return count_b
 
 
