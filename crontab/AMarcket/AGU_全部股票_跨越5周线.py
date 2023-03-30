6253a4dad69cbb499e00ccaa687d1890f8acdfb3
杨金鹏_18731 <18731@etransfar.com>
2023-03-30 09:32:01 +0800

    init

--- crontab/AMarcket/AGU_全部股票_跨越5周线.py
+++ crontab/AMarcket/AGU_全部股票_跨越5周线.py
@@ -13,7 +13,11 @@ import os
 project_name = 'QKL0731'
 rootPath = str(os.path.abspath(os.path.dirname(__file__)).split(project_name)[0]) + project_name
 sys.path.append(rootPath)
-print(rootPath)
+
+curPath1 = os.path.abspath(os.path.dirname(__file__))
+rootPath1 = os.path.split(curPath1)[0]
+sys.path.append(rootPath1)
+print(rootPath1)
 import common_image
 import common_mysqlUtil
 
