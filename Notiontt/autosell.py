0312c97c6577db4ed8dc069102093d4675042834
杨金鹏_18731 <18731@etransfar.com>
2023-08-29 22:58:04 +0800

    init

--- Notiontt/autosell.py
+++ Notiontt/autosell.py
@@ -26,18 +26,18 @@ def sell(code, price, count):
     click(coords=(300, 165))
     # 在文本框中输入全选与删除
     send_keys("^a")
-    time.sleep(0.2)
-    send_keys("{DELETE}")
     time.sleep(0.3)
+    send_keys("{DELETE}")
+    time.sleep(0.5)
     # 输入股票代码，这里是【变量】
     send_keys(code, pause=0.5)
 
     # 点击鼠标左键，进入卖出价格文本框
     click(coords=(300, 264))
     # 在文本框中输入全选与删除
-    time.sleep(0.1)
-    double_click(coords=(300, 264))
     time.sleep(0.2)
+    double_click(coords=(300, 264))
+    time.sleep(0.5)
     send_keys("{DELETE}")
     time.sleep(0.4)
     # 输入卖出价格，这里是【变量】
@@ -47,17 +47,17 @@ def sell(code, price, count):
     click(coords=(300, 325))
     # 在文本框中输入全选与删除
     send_keys("^a")
-    time.sleep(0.1)
+    time.sleep(0.4)
     send_keys("{DELETE}")
-    time.sleep(0.2)
+    time.sleep(0.6)
     # 输入要卖出的数量，这里是【变量】
-    send_keys(count, pause=0.2)
+    send_keys(count, pause=0.4)
     # 点击鼠标左键，点击卖出按钮
     click(coords=(400, 400))
-    time.sleep(0.15)
+    time.sleep(0.5)
     # 点击鼠标左键，点击卖出按钮
     click(coords=(904, 607))
-    time.sleep(0.2)
+    time.sleep(0.6)
     # 点击鼠标左键，点击卖出完成确认按钮
     click(coords=(1015, 557))
 
@@ -80,7 +80,7 @@ def maximize(title_str):
     app.connect(title=title_str, timeout=120)
     app[title_str].wrapper_object().maximize()
     print(app.windows())
-    time.sleep(0.1)
+    time.sleep(1)
 
 
 def minimize(title_str):
@@ -114,6 +114,7 @@ if __name__ == "__main__":
         for num in range(0, quotient):
             price_sell = round(price_float * (1 + 0.001 * num), 2)
             sell(code, str(price_sell), str(fenshu))
+            time.sleep(2)
 
         # 最小化窗口
         minimize(title_str)
