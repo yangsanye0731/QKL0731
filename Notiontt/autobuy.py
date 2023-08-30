325c1dc9721c460c2d25e32dc6144bf987020f1f
杨金鹏_18731 <18731@etransfar.com>
2023-08-30 22:12:55 +0800

    init

--- Notiontt/autobuy.py
+++ Notiontt/autobuy.py
@@ -22,15 +22,15 @@ import common
 def buy(code, price, count):
     # 在全屏模式下点击买入菜单
     click(coords=(70, 120))  # 在 (100, 200) 坐标处点击
-    random.randint(5, 10)
+    time.sleep(random.randint(5, 10))
     # 点击鼠标左键，进入证券代码文本输入框
     click(coords=(300, 165))
     # 在证券代码输入全选，并删除
     send_keys("^a")
     time.sleep(0.1)
     send_keys("{DELETE}")
-    time.sleep(0.2)
-    send_keys(code, pause=0.5)
+    time.sleep(round(random.uniform(0.2, 1), 1))
+    send_keys(code, pause=round(random.uniform(0.2, 1), 1))
     time.sleep(1)
 
     # 点击鼠标左键，进入买入价格文本框
@@ -38,27 +38,27 @@ def buy(code, price, count):
     # 在文本框中双击并删除
     time.sleep(0.2)
     double_click(coords=(300, 264))
-    time.sleep(0.1)
+    time.sleep(round(random.uniform(0.2, 1), 1))
     send_keys("{DELETE}")
-    time.sleep(0.1)
+    time.sleep(round(random.uniform(0.2, 1), 1))
     # 输入买入价格，这里是【变量】
-    send_keys(price, pause=0.1)
+    send_keys(price, pause=round(random.uniform(0.2, 1), 1))
 
     # 点击鼠标左键，进入买入数量行
     click(coords=(300, 325))
     # 在文本框中输入全选与删除
     send_keys("^a")
-    time.sleep(0.2)
+    time.sleep(round(random.uniform(0.2, 1), 1))
     send_keys("{DELETE}")
     time.sleep(0.3)
     # 输入要卖出的数量，这里是【变量】
-    send_keys(count, pause=0.1)
+    send_keys(count, pause=round(random.uniform(0.2, 1), 1))
     # 点击鼠标左键，点击卖出按钮
     click(coords=(400, 400))
-    time.sleep(0.2)
+    time.sleep(round(random.uniform(0.2, 1), 1))
     # 点击鼠标左键，点击卖出按钮
     click(coords=(904, 607))
-    time.sleep(0.1)
+    time.sleep(round(random.uniform(0.2, 1), 1))
     # 点击鼠标左键，点击卖出完成确认按钮
     click(coords=(1015, 557))
 
@@ -81,7 +81,7 @@ def maximize(title_str):
     app.connect(title=title_str, timeout=120)
     app[title_str].wrapper_object().maximize()
     print(app.windows())
-    time.sleep(0.2)
+    time.sleep(2)
 
 
 def minimize(title_str):
