c9839dcf2887665af6153163decf87bb42716927
杨金鹏_18731 <18731@etransfar.com>
2023-09-28 23:08:54 +0800

    init

--- Notiontt/client.py
+++ Notiontt/client.py
@@ -13,6 +13,11 @@ rootPath1 = os.path.split(curPath1)[0]
 sys.path.append(rootPath1)
 import common
 
+import warnings
+
+# Suppress the FutureWarning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 def execute_remote_command(command):
     conn = rpyc.connect("192.168.233.128", 18861)  # 替换为服务器的IP地址
@@ -24,20 +29,23 @@ def execute_remote_command(command):
     conn.close()
     return result
 
+
 string_input = "python "
-type_input = input("请输入您的信息：s、b、 d、mysql、o ：")
+type_input = input("请输入您要操作的类型：s、b、 d、mysql、o ：")
 if type_input == 'd':
     string_input = string_input + "autoDischarge.py"
+    print("操作命令：" + string_input)
     output = execute_remote_command(string_input)
     # print("Remote command output:")
     # print(output)
 elif type_input == 'o':
     string_input = string_input + "autoopen.py"
+    print("操作命令：" + string_input)
     output = execute_remote_command(string_input)
 elif type_input == 'mysql':
     string_input = string_input + "client_script.py"
 else:
-    code_input = input("请输入您的信息：")
+    code_input = input("请输入名称：")
     zhangdiefu, price = common.zhangdiefu_and_price(code_input)
     price_input = str(price)
     count_input = "1000"
@@ -46,7 +54,7 @@ else:
         string_input = string_input + "autosell.py " + code_input + " " + price_input + " " + count_input
     elif type_input == 'b':
         string_input = string_input + "autobuy.py " + code_input + " " + price_input + " " + count_input
-    print(string_input)
+    print("操作命令：" + string_input)
     output = execute_remote_command(string_input)
     # print("Remote command output:")
     # print(output)
