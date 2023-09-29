68076985a2b706b629c4aee6f09d5e7871f03d7f
杨金鹏_18731 <18731@etransfar.com>
2023-09-29 13:37:01 +0800

    init

---
+++ Notiontt/zx_client.py
@@ -0,0 +1,60 @@
+# client.py
+import rpyc
+
+import sys
+import os
+
+project_name = 'QKL0731'
+rootPath = str(os.path.abspath(os.path.dirname(__file__)).split(project_name)[0]) + project_name
+sys.path.append(rootPath)
+
+curPath1 = os.path.abspath(os.path.dirname(__file__))
+rootPath1 = os.path.split(curPath1)[0]
+sys.path.append(rootPath1)
+import common
+
+import warnings
+
+# Suppress the FutureWarning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+def execute_remote_command(command):
+    # conn = rpyc.connect("192.168.233.128", 18861)  # 替换为服务器的IP地址
+    conn = rpyc.connect("localhost", 18862)  # 替换为服务器的IP地址
+    conn._config['sync_request_timeout'] = 1200
+    remote_service = conn.root
+
+    result = remote_service.exposed_execute_command(command)
+    conn.close()
+    return result
+
+
+string_input = "python "
+type_input = input("请输入您要操作的类型：s、b、 d、mysql、o ：")
+if type_input == 'd':
+    string_input = string_input + "autoDischarge.py"
+    print("操作命令：" + string_input)
+    output = execute_remote_command(string_input)
+    # print("Remote command output:")
+    # print(output)
+elif type_input == 'o':
+    string_input = string_input + "autoopen.py"
+    print("操作命令：" + string_input)
+    output = execute_remote_command(string_input)
+elif type_input == 'mysql':
+    string_input = string_input + "client_script.py"
+else:
+    code_input = input("请输入名称：")
+    zhangdiefu, price = common.zhangdiefu_and_price(code_input)
+    price_input = str(price)
+    count_input = "1000"
+
+    if type_input == 's':
+        string_input = string_input + "autosell.py " + code_input + " " + price_input + " " + count_input
+    elif type_input == 'b':
+        string_input = string_input + "zx_autobuy.py " + code_input + " " + price_input + " " + count_input
+    print("操作命令：" + string_input)
+    output = execute_remote_command(string_input)
+    # print("Remote command output:")
+    # print(output)
---
+++ Notiontt/zx_server.py
@@ -0,0 +1,81 @@
+# server.py
+import rpyc
+import subprocess
+import time
+import datetime
+
+from notion_database.database import Database
+from notion_database.page import Page
+from notion_database.properties import Properties
+
+#######################################################################################################################
+# ############################################################################################### 配置程序应用所需要环境PATH
+import sys
+import os
+
+project_name = 'QKL0731'
+rootPath = str(os.path.abspath(os.path.dirname(__file__)).split(project_name)[0]) + project_name
+sys.path.append(rootPath)
+
+curPath1 = os.path.abspath(os.path.dirname(__file__))
+rootPath1 = os.path.split(curPath1)[0]
+sys.path.append(rootPath1)
+
+import logging
+
+# 配置日志输出格式和级别
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger().setLevel(logging.INFO)
+
+integrations_token = "secret_rxaAzcdjzdVq4pe1hrkqkhzxJlm2isBh96Z4rxdB9Cc"
+
+
+def create_content(database_id, title, operate, is_operate, create_time):
+    database = Database(
+        integrations_token=integrations_token
+    )
+    database.retrieve_database(
+        database_id=database_id, get_properties=True
+    )
+
+    PROPERTY = Properties()
+    PROPERTY.set_title("title", title)
+    PROPERTY.set_rich_text("操作", operate)
+    PROPERTY.set_rich_text("是否操作", is_operate)
+    PROPERTY.set_rich_text("时间", create_time)
+    P = Page(integrations_token=integrations_token)
+    P.create_page(database_id=database_id, properties=PROPERTY)
+
+
+class RemoteCommandsService(rpyc.Service):
+    def on_connect(self, conn):
+        print("Client connected.")
+
+    def on_disconnect(self, conn):
+        print("Client disconnected.")
+
+    def exposed_execute_command(self, command):
+        try:
+            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+            print(result)
+
+            if "autobuy.py" in command or "autosell.py" in command:
+                # 同步Notion
+                word_list = command.split()
+                create_content(database_id="c7d5a0173e1948e3a8a52a2af6411260", title=word_list[2], operate=command,
+                               is_operate='是', create_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # # 记录操作日志
+            # common_mysqlSSHUtil.insert_record(
+            #     "INSERT INTO operate (`code`, `operate`, `is_operate`, `gmt_create`) VALUES ('" + "code" + "', '" +
+            #     command + "', '" + "是" + "', '" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "');")
+            # # 返回信息
+            return result
+        except subprocess.CalledProcessError as e:
+            return f"Error executing command: {e.output}"
+
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+
+    server = ThreadedServer(RemoteCommandsService, port=18862)
+    server.start()
