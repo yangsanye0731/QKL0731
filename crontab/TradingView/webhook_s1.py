7ae867b8047619d80465d682eb4885c29c399799
杨金鹏_18731 <18731@etransfar.com>
2023-10-23 10:26:22 +0800

    init

--- crontab/TradingView/webhook_s1.py
+++ crontab/TradingView/webhook_s1.py
@@ -210,6 +210,22 @@ def state(ma10, sma10):
         item_state = "顶部"
     if ma10[-3] > ma10[-2] > ma10[-1]:
         item_state = "下降"
+        if ma10[-2] < ma10[-3]:
+            item_state = "下降1"
+            if ma10[-3] < ma10[-4]:
+                item_state = "下降2"
+                if ma10[-4] < ma10[-5]:
+                    item_state = "下降3"
+                    if ma10[-5] < ma10[-6]:
+                        item_state = "下降4"
+                        if ma10[-6] < ma10[-7]:
+                            item_state = "下降5"
+                            if ma10[-7] < ma10[-8]:
+                                item_state = "下降6"
+                                if ma10[-8] < ma10[-9]:
+                                    item_state = "下降7"
+                                    if ma10[-9] < ma10[-10]:
+                                        item_state < "下降8"
     if ma10[-3] > ma10[-2] < ma10[-1]:
         item_state = "底部🚀"
     if ma10[-1] > sma10[-1] and ma10[-2] < sma10[-2]:
